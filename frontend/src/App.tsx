a3bfb98fff58abe50460cf2430df65657b470811
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-03-29 16:58:58 +0900

    canvas で 図形 表示してみた

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
 
+import { Canvas } from "./Canvas";
+
 function App() {
     return (
         <>
             <h1>Header</h1>
             <hr />
+            <Canvas />
         </>
     );
 }
