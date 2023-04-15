0e9f8953d8dc153202a6a1b371980cb8d6c4a4b0
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-04-15 14:02:50 +0900

    show connect, disconnect msg

--- backend/src/events/events.gateway.ts
+++ backend/src/events/events.gateway.ts
@@ -6,6 +6,14 @@ import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
   },
 })
 export class EventsGateway {
+  handleConnection(client: any) {
+    console.log('handleConnection', client);
+  }
+
+  handleDisconnect(client: any) {
+    console.log('handleDisconnect', client);
+  }
+
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
     return 'Hello world!';
