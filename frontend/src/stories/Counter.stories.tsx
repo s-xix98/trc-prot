c767b5a419cf7d01e227a643473fd16f7a7f1020
wyoheiii <seikimatu4649@gmail.com>
2023-04-17 15:17:08 +0900

    test counter stories

---
+++ frontend/src/stories/Counter.stories.tsx
@@ -0,0 +1,14 @@
+import { Counter } from "../Counter";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta:Meta<typeof Counter> = {
+  title:'a',
+  component: Counter,
+}
+
+export default meta;
+
+type Story = StoryObj<typeof Counter>;
+
+export const FirstStroy:Story  = {}
+
