6032c4035f050c84b956c073ec38544b9c2bcafa
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-04-17 14:19:50 +0900

    add wsClients map

--- backend/src/events/events.gateway.ts
+++ backend/src/events/events.gateway.ts
@@ -1,17 +1,24 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 
+import { Socket } from 'socket.io';
+
 @WebSocketGateway({
   cors: {
     origin: '*',
   },
 })
 export class EventsGateway {
-  handleConnection(_client: any) {
+  wsClients = new Map<string, Socket>();
+
+  handleConnection(client: Socket) {
     console.log('handleConnection');
+    // TODO : assert cliend id has
+    this.wsClients.set(client.id, client);
   }
 
-  handleDisconnect(_client: any) {
+  handleDisconnect(client: Socket) {
     console.log('handleDisconnect');
+    this.wsClients.delete(client.id);
   }
 
   @SubscribeMessage('message')
