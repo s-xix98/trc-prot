6ca8a0f2dacb2521fd97106016ff54d03787ab52
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-04-18 06:46:14 +0900

    Container add flex

--- frontend/src/components/Layout/Container.tsx
+++ frontend/src/components/Layout/Container.tsx
@@ -4,6 +4,7 @@ export const Container = ({ children }: { children: ReactNode }) => {
     return (
         <div
             style={{
+                flex: "1",
                 display: "flex",
                 flexDirection: "column",
                 overflow: "hidden",
