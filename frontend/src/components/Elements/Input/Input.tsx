f6620e387b037a9993f473ebda2e1a619bdac6f4
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-04-17 14:12:15 +0900

    add Input elm

---
+++ frontend/src/components/Elements/Input/Input.tsx
@@ -0,0 +1,23 @@
+import { ChangeEvent } from "react";
+
+import TextField from "@mui/material/TextField";
+
+export const Input = ({
+    msg,
+    onChangeAct,
+}: {
+    msg: string;
+    onChangeAct: (e: ChangeEvent<HTMLInputElement>) => void;
+}) => {
+    return (
+        <>
+            <TextField
+                id="outlined-multiline-static"
+                multiline
+                value={msg}
+                onChange={onChangeAct}
+                style={{ height: "100%", width: "100%" }}
+            />
+        </>
+    );
+};
