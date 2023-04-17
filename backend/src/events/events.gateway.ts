682810b41fb2f25f1ba6a9cf221679eb10d137f0
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-04-17 14:21:34 +0900

    message ret rcv msg to all client

--- backend/src/events/events.gateway.ts
+++ backend/src/events/events.gateway.ts
@@ -22,7 +22,11 @@ export class EventsGateway {
   }
 
   @SubscribeMessage('message')
-  handleMessage(_client: any, _payload: any): string {
+  handleMessage(client: Socket, payload: string): string {
+    console.log('message', payload);
+    for (const c of this.wsClients.values()) {
+      c.emit('message', `${client.id.substring(0, 4)} > ${payload}`);
+    }
     return 'Hello world!';
   }
 }
