7340664e072b618be6b84b11c3478328544c67be
wyoheiii <seikimatu4649@gmail.com>
2023-04-20 00:09:41 +0900

    とりあえず雑にpostだけ対応

--- backend/src/post-message/post-message.controller.ts
+++ backend/src/post-message/post-message.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Post } from '@nestjs/common';
+import { PostMessageService } from './post-message.service';
+import { MessageDto } from './dto/message.dto';
+import { Message } from '@prisma/client';
+import { Body } from '@nestjs/common';
 @Controller('post-message')
-export class PostMessageController {}
+export class PostMessageController {
+  constructor(private readonly postMessageService: PostMessageService) {}
+
+  // { //JSON
+  // "content": "a",
+  // "authorId": 1
+  // }
+  @Post()
+  createMessage(@Body() dto: MessageDto): Promise<Message> {
+    return this.postMessageService.postMessage(dto);
+  }
+}
