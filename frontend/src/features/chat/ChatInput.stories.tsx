99c4d0b52fb3e9f489e01bf5a886c798f3fec5ec
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-04-20 06:46:02 +0900

    storybook add Chat

---
+++ frontend/src/features/chat/ChatInput.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { ChatInput } from "./ChatInput";
+
+const meta = {
+    component: ChatInput,
+    tags: ["autodocs"],
+} satisfies Meta<typeof ChatInput>;
+
+export default meta;
+
+type Story = StoryObj<typeof ChatInput>;
+
+export const Basic: Story = {};
