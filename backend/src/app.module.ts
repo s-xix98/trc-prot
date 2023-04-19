0c143dff32933316122d215897a91aacea664a5e
wyoheiii <seikimatu4649@gmail.com>
2023-04-19 21:58:18 +0900

    create prisma service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsGateway } from './events/events.gateway';
 import { PostMessageModule } from './post-message/post-message.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PostMessageModule],
+  imports: [PostMessageModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService, EventsGateway],
 })
