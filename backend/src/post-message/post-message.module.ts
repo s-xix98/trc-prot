956d1b199c2c2265c534703f1e77886c22ad9e99
wyoheiii <seikimatu4649@gmail.com>
2023-04-19 22:18:28 +0900

    export prisma module

--- backend/src/post-message/post-message.module.ts
+++ backend/src/post-message/post-message.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PostMessageController } from './post-message.controller';
 import { PostMessageService } from './post-message.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [PostMessageController],
   providers: [PostMessageService],
 })
