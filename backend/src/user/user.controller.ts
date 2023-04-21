0771456ea1e40ad0edeb1b0140e97b729b21e3a8
wyoheiii <seikimatu4649@gmail.com>
2023-04-21 23:08:05 +0900

    signupへのリクエスト時のルーティング処理とロジック部分の呼び出し

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { signUpDto } from './dto/signUp.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('signup')
+  signUp(@Body() dto: signUpDto) {
+    this.userService.signUp(dto);
+  }
+}
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { signUpDto } from './dto/signUp.dto';
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
+
+  async signUp(dto: signUpDto) {}
 }
