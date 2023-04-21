95a23d1f50ecb3801730934b78d30c744c2f359f
wyoheiii <seikimatu4649@gmail.com>
2023-04-21 23:27:09 +0900

    signup時のロジック部分

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -5,5 +5,24 @@ import { signUpDto } from './dto/signUp.dto';
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
-  async signUp(dto: signUpDto) {}
+  async signUp(dto: signUpDto) {
+    // エラー処理は書いてない
+    console.log(dto);
+    const user = this.prisma.user.create({
+      data: {
+        email: dto.email,
+        nickname: dto.nickname,
+        // 今後ハッシュ化
+        hashedPassword: dto.hashedPassword,
+      },
+    });
+    user
+      .then((e) => {
+        console.log(e);
+      })
+      .catch((e) => {
+        // 失敗時には'P2002'が帰ってくる
+        console.log(e);
+      });
+  }
 }
