a78e408f62d1d6144b8c3786c2f0b35b3c4282a8
wyoheiii <seikimatu4649@gmail.com>
2023-04-21 20:26:24 +0900

    dbを操作するからPrismaClientのラッパークラスをimportした

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
-
+import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+}
