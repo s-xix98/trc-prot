eddae32f00985e67293953187cf503523917b426
wyoheiii <seikimatu4649@gmail.com>
2023-04-22 20:55:17 +0900

    jestを使うために相対パスに変更した 絶対パスだとpackage.jsをいじらないといけなさそうなのでやめといた

--- backend/src/post-message/post-message.service.spec.ts
+++ backend/src/post-message/post-message.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostMessageService } from './post-message.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('PostMessageService', () => {
   let service: PostMessageService;
--- backend/src/post-message/post-message.service.ts
+++ backend/src/post-message/post-message.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { Message } from '@prisma/client';
 import { MessageDto } from './dto/message.dto';
 
