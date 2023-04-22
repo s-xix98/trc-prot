50c15faadc0b7d873b2b9e1e3f781efb9abe895d
nori <nori623p@gmail.com>
2023-04-22 07:13:34 +0200

    コールバックを受け取るだけのボタン機能を追加

---
+++ frontend/src/components/Elements/Button/BasicButton.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react';
+import Button from '@mui/material/Button';
+
+export const BasicButton = ({
+  btnAct,
+  children,
+}: {
+  btnAct: () => void;
+  children: ReactNode;
+}) => {
+  return <Button size={"small"} onClick={btnAct}>{children}</Button>;
+};
