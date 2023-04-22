367dce6723fba886933aa904a3000b9d8a9505f8
wyoheiii <seikimatu4649@gmail.com>
2023-04-22 21:00:04 +0900

    PrismaServiceとdbのMessageテーブルのmockデータを作った

--- backend/src/post-message/post-message.service.spec.ts
+++ backend/src/post-message/post-message.service.spec.ts
@@ -2,6 +2,27 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PostMessageService } from './post-message.service';
 import { PrismaService } from '../prisma/prisma.service';
 
+type Message = {
+  id: number;
+  createdAt: Date;
+  updatedAt: Date;
+  content: string;
+  authorId: number;
+};
+const mockMsg: Message = {
+  id: 12,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  content: 'nori',
+  authorId: 1,
+};
+
+const mockPrismaService = {
+  message: {
+    // PrismaService.createをmockMsgを返すだけの関数にした
+    create: jest.fn().mockReturnValue(mockMsg),
+  }
+};
 describe('PostMessageService', () => {
   let service: PostMessageService;
 
