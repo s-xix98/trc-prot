9839dad3e7d40718a5af11640fbde6eb7b29d8f1
wyoheiii <seikimatu4649@gmail.com>
2023-04-25 17:52:57 +0900

    swaggarでリクエストを送る時のデフォルトの値を設定した

--- backend/src/user/dto/user.dto.ts
+++ backend/src/user/dto/user.dto.ts
@@ -1,10 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class signUpDto {
+  @ApiProperty({ example: 'a@a.com', description: 'test' })
   email: string;
+  @ApiProperty({ example: 'nori', description: 'test' })
   nickname: string;
+  @ApiProperty({ example: 'abc', description: 'test' })
   hashedPassword: string;
 }
 
 export class loginDto {
+  @ApiProperty({ example: 'a@a.com', description: 'test' })
   email: string;
+  @ApiProperty({ example: 'abc', description: 'test' })
   hashedPassword: string;
 }
