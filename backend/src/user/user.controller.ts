cba33a49b1d36796d33da1d5d2ccb2b19045713e
wyoheiii <seikimatu4649@gmail.com>
2023-04-25 17:56:40 +0900

    swaggerのデフォルトbodyをlogin,signupに入るようにした *buildに失敗したからreturn周り修正した

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,17 +1,21 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
 import { loginDto, signUpDto } from './dto/user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
+@ApiTags('/user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('signup')
+  @ApiOperation({ summary: 'signUp nori' })
   signUp(@Body() dto: signUpDto) {
     this.userService.signUp(dto);
   }
   @Post('login')
+  @ApiOperation({ summary: 'login nori' })
   login(@Body() dto: loginDto) {
     this.userService.login(dto);
   }
