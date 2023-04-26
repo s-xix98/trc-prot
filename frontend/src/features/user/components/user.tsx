dd7ce00d92394d7cd55394a4a76e8c884811d922
nori <nori623p@gmail.com>
2023-04-26 09:40:03 +0200

    LoginフォームのUI追加

--- frontend/src/features/user/components/user.tsx
+++ frontend/src/features/user/components/user.tsx
@@ -1,10 +1,19 @@
 import { ReactNode } from 'react';
 import { Container } from '@/components/Layout/Container';
 
+import { Login } from './login';
+
 export const User = ({ children }: { children: ReactNode }) => {
   return (
     <Container flexDirection="column">
-      <h1>UserArea</h1>
+      <div>
+        <Container flexDirection="row">
+          <h1>UserArea</h1>
+          <div style={{ margin: 'auto 10px auto auto' }}>
+            <Login />
+          </div>
+        </Container>
+      </div>
       <hr />
       {children}
     </Container>
