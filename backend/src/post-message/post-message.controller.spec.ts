3b6111a4a8b166e6be37089f5f313c6174f4132b
wyoheiii <seikimatu4649@gmail.com>
2023-04-26 20:24:52 +0900

    controllerに依存しているproviderをimportした

--- backend/src/post-message/post-message.controller.spec.ts
+++ backend/src/post-message/post-message.controller.spec.ts
@@ -3,6 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TestModule } from '../test/test.module';
 
 import { PostMessageController } from './post-message.controller';
+import { PostMessageService } from './post-message.service';
 
 describe('PostMessageController', () => {
   let controller: PostMessageController;
@@ -11,6 +12,7 @@ describe('PostMessageController', () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [TestModule],
       controllers: [PostMessageController],
+      providers: [PostMessageService],
     }).compile();
 
     controller = module.get<PostMessageController>(PostMessageController);
--- backend/src/user/user.controller.spec.ts
+++ backend/src/user/user.controller.spec.ts
@@ -3,6 +3,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TestModule } from '../test/test.module';
 
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -11,6 +12,7 @@ describe('UserController', () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [TestModule],
       controllers: [UserController],
+      providers: [UserService],
     }).compile();
 
     controller = module.get<UserController>(UserController);
