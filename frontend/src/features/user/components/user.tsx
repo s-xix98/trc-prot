2185f8c4187a41a59be306853e63df2e383b30a6
nori <nori623p@gmail.com>
2023-04-27 02:20:36 +0200

    refactor

--- frontend/src/features/user/components/user.tsx
+++ frontend/src/features/user/components/user.tsx
@@ -9,18 +9,25 @@ import { LoginForm } from './login';
 export const User = ({ children }: { children: ReactNode }) => {
   const [userInfo, setUserInfo] = useState<UserInfo>();
 
+  const UserInputArea = () => {
+    if (userInfo) {
+      return (
+        <p>
+          id : {userInfo?.id}, name : {userInfo?.nickname}
+        </p>
+      );
+    } else {
+      return <LoginForm setUserInfo={setUserInfo} />;
+    }
+  };
+
   return (
     <Container flexDirection="column">
       <div>
         <Container flexDirection="row">
           <h1>UserArea</h1>
           <div style={{ margin: 'auto 10px auto auto' }}>
-            {userInfo && (
-              <p>
-                id : {userInfo?.id}, name : {userInfo?.nickname}
-              </p>
-            )}
-            {!userInfo && <LoginForm setUserInfo={setUserInfo} />}
+            <UserInputArea />
           </div>
         </Container>
       </div>
