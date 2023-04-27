05ddf1af673ec1c98fdc278cb03febadcf139336
nori <nori623p@gmail.com>
2023-04-27 03:56:32 +0200

    テキストがずれたのでセンタリング

--- frontend/src/features/user/components/user.tsx
+++ frontend/src/features/user/components/user.tsx
@@ -32,7 +32,15 @@ export const User = ({ children }: { children: ReactNode }) => {
     <Container flexDirection="column">
       <div>
         <Container flexDirection="row">
-          <h1>UserArea</h1>
+          <h1
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+            }}
+          >
+            UserArea
+          </h1>
           <div style={{ margin: 'auto 10px auto auto' }}>
             <UserInputArea />
           </div>
