d910b7161fef69c5147600ced1ab0461163f6841
nori <nori623p@gmail.com>
2023-04-27 03:43:43 +0200

    SignUpFormのstubを配置

---
+++ frontend/src/features/user/components/SignUp.tsx
@@ -0,0 +1,10 @@
+import { UserInfo } from '../types/UserDto';
+
+// stub
+export const SignUpForm = ({
+  setUserInfo,
+}: {
+  setUserInfo: (v: UserInfo) => void;
+}) => {
+  return <p>SignUP</p>;
+};
