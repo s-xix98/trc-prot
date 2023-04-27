55339d654234f78b7eefbbe84016a31ccec93c75
nori <nori623p@gmail.com>
2023-04-27 04:53:37 +0200

    signupフォームの入力をbackendにpostする機能を実装

--- frontend/src/features/user/api/userSignUp.tsx
+++ frontend/src/features/user/api/userSignUp.tsx
@@ -1,6 +1,7 @@
-import { UserInfo } from '../types/UserDto';
+import axios, { AxiosResponse } from 'axios';
 
-// stub
+import { SignUpDto, UserInfo } from '../types/UserDto';
+
 export const userSignUp = async (
   nickname: string,
   email: string,
@@ -8,5 +9,24 @@ export const userSignUp = async (
   setUserInfo: (v: UserInfo) => void,
 ) => {
   console.log('post /user/signup');
-  alert(`nickname: ${nickname}\nemail: ${email}\npassword: ${passwd}`);
+  const url = 'http://localhost:8000/user/signup';
+
+  // TODO validation フロントとバックどっちの責任？
+  const signUpDto: SignUpDto = {
+    nickname: nickname,
+    email: email,
+    hashedPassword: passwd,
+  };
+
+  // postでブロックしても問題ないならasync awaitでもいいかも
+  axios
+    .post<UserInfo>(url, signUpDto)
+    .then((res: AxiosResponse<UserInfo>) => {
+      console.log('signup res:', res);
+      setUserInfo(res.data);
+    })
+    .catch((err) => {
+      // TODO とりあえずなにもしない
+      console.log('signup err:', err);
+    });
 };
