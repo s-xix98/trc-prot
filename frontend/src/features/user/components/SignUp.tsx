b38e5febf62917a4aee2866ac5306500a293f66f
nori <nori623p@gmail.com>
2023-04-27 03:48:20 +0200

    SignUpForm実装。ほぼloginFormからコピペ、nicknameだけ追加

--- frontend/src/features/user/components/SignUp.tsx
+++ frontend/src/features/user/components/SignUp.tsx
@@ -1,10 +1,44 @@
+import { FormEventHandler } from 'react';
+
 import { UserInfo } from '../types/UserDto';
 
-// stub
+import { userSignUp } from '../api/userSignUp';
+
 export const SignUpForm = ({
   setUserInfo,
 }: {
   setUserInfo: (v: UserInfo) => void;
 }) => {
-  return <p>SignUP</p>;
+  const handleSignUp: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    console.log('signUpButton');
+    const nickname = (event.target as HTMLFormElement).nickname.value;
+    const email = (event.target as HTMLFormElement).email.value;
+    const password = (event.target as HTMLFormElement).password.value;
+    userSignUp(nickname, email, password, setUserInfo);
+  };
+
+  return (
+    <form onSubmit={handleSignUp}>
+      <input
+        type="text"
+        name="nickname"
+        placeholder="nickname"
+        style={{ marginRight: '5px' }}
+      />
+      <input
+        type="text"
+        name="email"
+        placeholder="email"
+        style={{ marginRight: '5px' }}
+      />
+      <input
+        type="text"
+        name="password"
+        placeholder="password"
+        style={{ marginRight: '10px' }}
+      />
+      <input type="submit" value="SignUp" />
+    </form>
+  );
 };
