b4e1bfe343a297589163027e5139aa9e1af6f1a1
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-04-28 10:13:12 +0900

    not -> !

--- frontend/scripts/create_storybook_file.sh
+++ frontend/scripts/create_storybook_file.sh
@@ -1,5 +1,5 @@
 set -e
 
-for file in $(find src -name '*.tsx' -not -name '*.stories.tsx' | grep -v src/stories | grep -v main.tsx); do
+for file in $(find src -name '*.tsx' ! -name '*.stories.tsx' | grep -v src/stories | grep -v main.tsx); do
 	touch ${file%.tsx}.stories.tsx
 done
