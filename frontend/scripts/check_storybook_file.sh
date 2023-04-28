9edfbb40c7b8be742ee77684e1f33c32ac041e25
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-04-28 10:23:41 +0900

    add type f

--- frontend/scripts/check_storybook_file.sh
+++ frontend/scripts/check_storybook_file.sh
@@ -1,5 +1,5 @@
 set -e
 
-for file in $(find src -name '*.tsx' ! -name '*.stories.tsx' | grep -v src/stories | grep -v main.tsx); do
+for file in $(find src -type f -name '*.tsx' ! -name '*.stories.tsx' | grep -v src/stories | grep -v main.tsx); do
 	echo ${file} && test -f ${file%.tsx}.stories.tsx
 done
