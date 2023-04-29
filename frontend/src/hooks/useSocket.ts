732ff0d98d5316ddff737927f8c04043a01f3fa6
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-04-29 18:53:47 +0900

    any で エラーが 出るので 無効化

--- frontend/src/hooks/useSocket.ts
+++ frontend/src/hooks/useSocket.ts
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { socket } from '@/socket';
 
 // TODO : できれば any 使いたくないが どう書けばいいんだ？
+// eslint-disable-next-line
 export const useSocket = (ev: string, listener: (...args: any[]) => void) => {
   useEffect(() => {
     socket.on(ev, listener);
