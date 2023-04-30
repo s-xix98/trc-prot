1a87a3d4bf0fef307ef87d5bdb311aa314749c1b
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-05-01 00:19:31 +0900

    any を 許容, コメント削除

--- frontend/src/hooks/useSocket.ts
+++ frontend/src/hooks/useSocket.ts
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 
 import { socket } from '@/socket';
 
-// TODO : できれば any 使いたくないが どう書けばいいんだ？
 // eslint-disable-next-line
 export const useSocket = (ev: string, listener: (...args: any[]) => void) => {
   useEffect(() => {
