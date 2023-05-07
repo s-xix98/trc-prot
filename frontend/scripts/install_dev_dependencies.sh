02a84dd37adf20c33cf9565f5b640e657daac563
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-05-08 08:44:50 +0900

    install dev dependencies

--- frontend/scripts/install_dev_dependencies.sh
+++ frontend/scripts/install_dev_dependencies.sh
@@ -2,3 +2,8 @@ set -eux
 
 # storybook
 # npx storybook@latest init
+npm install --save-dev @storybook/test-runner
+npm install --save-dev jest-image-snapshot
+
+# wait-on
+npm install --save-dev wait-on
