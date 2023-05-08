d9d2285a208739eeba814cc5d738b51d5e50fbfc
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-05-08 09:46:07 +0900

    install prettier, eslint plugin

--- frontend/scripts/install_dev_dependencies.sh
+++ frontend/scripts/install_dev_dependencies.sh
@@ -10,3 +10,15 @@ npm install --save-dev jest-image-snapshot
 
 # wait-on
 npm install --save-dev wait-on
+
+# prettier, eslint
+npm install --save-dev prettier
+npm install --save-dev eslint-config-prettier
+
+# https://typescript-eslint.io/getting-started/
+npm install --save-dev @typescript-eslint/parser @typescript-eslint/eslint-plugin
+
+npm install --save-dev eslint-plugin-react
+npm install --save-dev eslint-plugin-react-hooks
+
+npm install --save-dev eslint-plugin-import
