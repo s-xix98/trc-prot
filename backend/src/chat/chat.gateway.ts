6d8745a83e7eac41045e22fc1a7abe8207807753
wyoheiii <seikimatu4649@gmail.com>
2023-05-16 01:31:04 +0900

    messageをdbに保存できるようにした

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -1,17 +1,27 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
-
+import { PrismaService } from '../prisma/prisma.service';
 @WebSocketGateway({
   cors: {
     origin: '*',
   },
 })
 export class ChatGateway {
-
+  constructor(private prisma: PrismaService) {}
   handleConnection(client: Socket) {
     console.log('chat Connection');
   }
   handleDisconnect(client: Socket) {
     console.log('chat Disconnection');
+  }
+  @SubscribeMessage('sendMessage')
+  async sendMessage(client: Socket) {
+    await this.prisma.message.create({
+      data: {
+        content: 'test',
+        userId: 1,
+        chatRoomId: 1,
+      },
+    });
   }
 }
--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
 import { ChatController } from './chat.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 
 @Module({
+  imports: [PrismaModule],
   providers: [ChatService, ChatGateway],
   controllers: [ChatController],
 })
