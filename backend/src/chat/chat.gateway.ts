c293e713e6a2f263305b1d5dd703575a6ab12598
wyoheiii <seikimatu4649@gmail.com>
2023-05-16 02:02:17 +0900

    Channelに追加できる関数を実装した

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -17,6 +17,16 @@ export class ChatGateway {
   handleDisconnect(client: Socket) {
     console.log('chat Disconnection');
   }
+  @SubscribeMessage('joinChannel')
+  async joinChannel(client: Socket){
+    await this.prisma.roomMember.create({
+      data: {
+        userId: 1,
+        chatRoomId: 1,
+      },
+    });
+  }
+
   @SubscribeMessage('sendMessage')
   async sendMessage(client: Socket, messageDto: MessageDto) {
     const msg = await this.prisma.message.create({
