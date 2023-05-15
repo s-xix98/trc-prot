ad4f6a9281b3ec61f1fd79311edd450745fa206f
wyoheiii <seikimatu4649@gmail.com>
2023-05-16 01:38:20 +0900

    messageを受け取るdtoを作成、それを割り当てた

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -1,6 +1,7 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 import { PrismaService } from '../prisma/prisma.service';
+import { MessageDto } from './dto/message.dto';
 @WebSocketGateway({
   cors: {
     origin: '*',
@@ -15,12 +16,12 @@ export class ChatGateway {
     console.log('chat Disconnection');
   }
   @SubscribeMessage('sendMessage')
-  async sendMessage(client: Socket) {
+  async sendMessage(client: Socket, messageDto: MessageDto) {
     await this.prisma.message.create({
       data: {
-        content: 'test',
-        userId: 1,
-        chatRoomId: 1,
+        content: messageDto.content,
+        userId: messageDto.userId,
+        chatRoomId: messageDto.chatRoomId,
       },
     });
   }
