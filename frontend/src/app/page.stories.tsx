7b98440655b30afa2679f67d1108760e2d4ef4fd
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-05-28 01:00:33 +0900

    テストケースを追加

--- frontend/src/app/page.stories.tsx
+++ frontend/src/app/page.stories.tsx
@@ -29,3 +29,68 @@ export const Login: Story = {
     expect(canvas.getByText('ChatChannelArea'));
   },
 };
+
+export const SelectChannel: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const loginBtn = canvas.getByText('login as fuga');
+    await userEvent.click(loginBtn);
+    await sleep(3000);
+
+    const hoge0 = canvas.getByText('hoge 0');
+    await userEvent.click(hoge0);
+  },
+};
+
+export const SendMsg: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const loginBtn = canvas.getByText('login as fuga');
+    await userEvent.click(loginBtn);
+    await sleep(3000);
+
+    const hoge0 = canvas.getByText('hoge 0');
+    await userEvent.click(hoge0);
+
+    const inputElem =
+      canvas.getByTestId('input-test-id')?.firstElementChild?.firstElementChild;
+    if (inputElem === undefined || inputElem === null) {
+      expect(false);
+      return;
+    }
+    await userEvent.type(inputElem, 'This is test msg');
+    await userEvent.click(canvas.getByText('Send'));
+    // SEND ボタンに アニメーションがあり、スクショのタイミングによって
+    // スクショに若干の差異が生まれ テストが落ちてしまうので 適当に Footer を クリック
+    await userEvent.click(canvas.getByText('Footer'));
+  },
+};
+
+export const SendSomeMsg: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const loginBtn = canvas.getByText('login as fuga');
+    await userEvent.click(loginBtn);
+    await sleep(3000);
+
+    const hoge0 = canvas.getByText('hoge 0');
+    await userEvent.click(hoge0);
+
+    const inputElem =
+      canvas.getByTestId('input-test-id')?.firstElementChild?.firstElementChild;
+    if (inputElem === undefined || inputElem === null) {
+      expect(false);
+      return;
+    }
+    for (let i = 0; i < 30; i++) {
+      await userEvent.type(inputElem, `This is test msg ${i}`);
+      await userEvent.click(canvas.getByText('Send'));
+    }
+    // SEND ボタンに アニメーションがあり、スクショのタイミングによって
+    // スクショに若干の差異が生まれ テストが落ちてしまうので 適当に Footer を クリック
+    await userEvent.click(canvas.getByText('Footer'));
+  },
+};
