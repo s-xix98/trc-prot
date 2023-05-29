dfec4d54ae85e856d98c2fb788cdc5646e444c5c
wyoheiii <seikimatu4649@gmail.com>
2023-05-29 21:49:08 +0900

    testリファクタ

--- backend/src/chat/chat.gateway.spec.ts
+++ backend/src/chat/chat.gateway.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { io, Socket } from 'socket.io-client';
-import { ChatRoom, Message, Prisma, PrismaClient, RoomMember, User } from '@prisma/client';
+import { ChatRoom, User } from '@prisma/client';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { INestApplication } from '@nestjs/common';
 
@@ -10,19 +10,20 @@ import { PrismaService } from '../prisma/prisma.service';
 import { CreateChannelDto, JoinChannelDto } from './dto/Channel.dto';
 import { ChatGateway } from './chat.gateway';
 import { MessageDto } from './dto/message.dto';
+
 type testUser = {
   user: User;
   socket: Socket;
-}
+};
+const modelNames = ['chatRoom', 'user'];
 const USERNUM = 10;
 
 const createTestUsers = async (prismaService: PrismaService) => {
   const testUsers: testUser[] = [];
   for (let i = 0; i < USERNUM; i++) {
-
     const sock: Socket = io('http://localhost:8001');
 
-    const user:User = await prismaService.user.upsert({
+    const user: User = await prismaService.user.upsert({
       where: {
         email: `chatTestUser${i}@test.com`,
       },
@@ -39,27 +40,29 @@ const createTestUsers = async (prismaService: PrismaService) => {
   return testUsers;
 };
 
-const cleanupDatabase = async (prisma:PrismaService): Promise<void> => {
-
-  const modelNames = Prisma.dmmf.datamodel.models.map((model) => model.name);
+const cleanupDatabase = async (
+  modelNames: string[],
+  prisma: PrismaService,
+): Promise<void> => {
   console.log(modelNames);
   await Promise.all(
-    // @ts-ignore
-    // prisma. user prisma.chatroom 的なのになる
-    modelNames.map((modelName) => prisma[modelName.toLowerCase()].deleteMany())
+    // prisma.user prisma.chatroom 的なのになる
+    modelNames.map(
+      async (modelName) => await (prisma as any)[modelName].deleteMany({}),
+    ),
   );
-
-  prisma.$disconnect();
 };
 
-const emitAndWaitForEvent = async <T>(eventName:string, socket:Socket, dto:T) => {
+const emitAndWaitForEvent = async <T>(
+  eventName: string,
+  socket: Socket,
+  dto: T,
+) => {
   return new Promise((resolve) => {
     socket.on(eventName, async () => resolve(null));
     socket.emit(eventName, dto);
   });
-}
-
-
+};
 
 describe('ChatGateway', () => {
   let gateway: ChatGateway;
@@ -89,7 +92,6 @@ describe('ChatGateway', () => {
         console.log(`connected ${testUser.user.username}`);
       });
     });
-
   });
   afterAll(async () => {
     testUsers.map((testUser) => {
@@ -103,21 +105,7 @@ describe('ChatGateway', () => {
     });
 
     await app.close();
-    // await cleanupDatabase(prismaService);
-
-
-    await prismaService.chatRoom.deleteMany({
-      where: {
-        roomName: room?.roomName || 'testroom',
-      },
-    });
-    for (let i = 0; i < USERNUM; i++) {
-      await prismaService.user.delete({
-        where: {
-          id: testUsers[i].user.id,
-        },
-      });
-    }
+    await cleanupDatabase(modelNames, prismaService);
   });
 
   test('should be defined', () => {
@@ -125,14 +113,17 @@ describe('ChatGateway', () => {
   });
 
   describe('create Channel', () => {
-
     test('users[0]が部屋を作成', async () => {
       const user: testUser = testUsers[0];
       const createChannelDto: CreateChannelDto = {
         roomName: 'testroom',
         userId: user.user.id,
       };
-      await emitAndWaitForEvent<CreateChannelDto>('createChannel', user.socket, createChannelDto);
+      await emitAndWaitForEvent<CreateChannelDto>(
+        'createChannel',
+        user.socket,
+        createChannelDto,
+      );
 
       room = await prismaService.chatRoom.findFirst({
         where: {
@@ -145,7 +136,6 @@ describe('ChatGateway', () => {
   });
 
   describe('join Channel', () => {
-
     let roomId: number;
     beforeEach(() => {
       if (!room) {
@@ -155,30 +145,33 @@ describe('ChatGateway', () => {
     });
 
     test('users[1]~users[9]が部屋に参加', async () => {
-      const promises:Promise<unknown>[] = [];
+      const promises: Promise<unknown>[] = [];
 
-        testUsers.slice(1).map((testUser) => {
-          const joinChannel: JoinChannelDto = {
-            userId: testUser.user.id,
-            chatRoomId: roomId,
-          };
-          const joinPromise = emitAndWaitForEvent<JoinChannelDto>('joinChannel', testUser.socket, joinChannel);
-          promises.push(joinPromise);
-        });
-
-        await Promise.all(promises).then( async () => {
-          const MemberList = await prismaService.roomMember.findMany({
-            where: {
-              chatRoomId: roomId,
-            },
-          });
-          expect(MemberList.length).toEqual(USERNUM);
-        });
+      testUsers.slice(1).map((testUser) => {
+        const joinChannel: JoinChannelDto = {
+          userId: testUser.user.id,
+          chatRoomId: roomId,
+        };
+        const joinPromise = emitAndWaitForEvent<JoinChannelDto>(
+          'joinChannel',
+          testUser.socket,
+          joinChannel,
+        );
+        promises.push(joinPromise);
       });
+
+      await Promise.all(promises).then(async () => {
+        const MemberList = await prismaService.roomMember.findMany({
+          where: {
+            chatRoomId: roomId,
+          },
+        });
+        expect(MemberList.length).toEqual(USERNUM);
+      });
+    });
   });
 
   describe('sendMessage', () => {
-
     let roomId: number;
     beforeEach(() => {
       if (!room) {
@@ -195,7 +188,11 @@ describe('ChatGateway', () => {
         chatRoomId: roomId,
       };
 
-      await emitAndWaitForEvent<MessageDto>('sendMessage', user.socket, messageDto);
+      await emitAndWaitForEvent<MessageDto>(
+        'sendMessage',
+        user.socket,
+        messageDto,
+      );
       const dbMsg = await prismaService.message.findFirst({
         where: {
           chatRoomId: roomId,
@@ -205,7 +202,6 @@ describe('ChatGateway', () => {
       expect(dbMsg?.content).toEqual(messageDto.content);
     });
 
-
     test('users[0]が送信したメッセージが全員に送信されるか', async () => {
       const user = testUsers[0];
       const messageDto: MessageDto = {
@@ -213,13 +209,13 @@ describe('ChatGateway', () => {
         userId: user.user.id,
         chatRoomId: roomId,
       };
-      const promises:Promise<unknown>[] = []
+      const promises: Promise<unknown>[] = [];
 
       let receivedCount = 0;
 
       testUsers.map((user) => {
         const joinPromise = new Promise((resolve) => {
-          user.socket.on('sendMessage', async () =>{
+          user.socket.on('sendMessage', async () => {
             receivedCount++;
             console.log(receivedCount);
             resolve(null);
