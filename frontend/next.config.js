ee848ad15d536aa6891c49199f33521661cde2cb
wyoheiii <seikimatu4649@gmail.com>
2023-05-30 06:41:41 +0900

    fmt

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler:{
+  compiler: {
     styledComponents: true,
-  }
+  },
 };
 
 module.exports = nextConfig;
