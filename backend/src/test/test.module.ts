59f9a3343f0142cab7c68f5904e022b89c6a312a
wyoheiii <seikimatu4649@gmail.com>
2023-05-31 15:27:29 +0900

    testServiceを自動生成 nest g service test

--- backend/src/test/test.module.ts
+++ backend/src/test/test.module.ts
@@ -3,10 +3,11 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { PrismaModule } from '../prisma/prisma.module';
 import { PrismaService } from '../prisma/prisma.service';
+import { TestService } from './test.service';
 
 @Module({
   imports: [PrismaModule, ConfigModule],
-  providers: [PrismaService, ConfigService],
+  providers: [PrismaService, ConfigService, TestService],
   exports: [PrismaService, ConfigService],
 })
 export class TestModule {}
