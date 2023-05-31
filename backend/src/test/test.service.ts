df72afd21ca0b3bf2fa109d62618c4b851483f4e
wyoheiii <seikimatu4649@gmail.com>
2023-05-31 17:07:46 +0900

    testでprisma使うからprisma importした

--- backend/src/test/test.service.ts
+++ backend/src/test/test.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class TestService {}
+export class TestService {
+  constructor(private prismaService: PrismaService) {}
