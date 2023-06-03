f2d771a95bf212147441a2031bd80c60141d0463
nori <nori623p@gmail.com>
2023-06-03 06:54:34 +0200

    中心に●を描画

--- frontend/src/features/game/Game.tsx
+++ frontend/src/features/game/Game.tsx
@@ -1,12 +1,36 @@
 'use client';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+const DrawBall = (
+  ctx: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  radius: number,
+) => {
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, Math.PI * 2);
+  ctx.fillStyle = 'red';
+  ctx.fill();
+  ctx.closePath();
+};
 
 const Game = () => {
+  const [ctx, setContext] = useState<CanvasRenderingContext2D | null>(null);
+
   useEffect(() => {
     const canvas = document.getElementById('canvas') as HTMLCanvasElement;
     canvas.style.border = '4px solid';
     canvas.style.color = 'black';
+    const canvasContext = canvas.getContext('2d');
+    setContext(canvasContext);
   }, []);
+
+  useEffect(() => {
+    if (ctx === null) {
+      return;
+    }
+    DrawBall(ctx, 400 / 2, 400 / 2, 10);
+  });
 
   return <canvas width={400} height={400} id={'canvas'}></canvas>;
 };
