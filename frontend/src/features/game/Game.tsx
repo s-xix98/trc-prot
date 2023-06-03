d78024c606bf6314bf24aee5dee68655bcdc1400
nori <nori623p@gmail.com>
2023-06-03 06:37:19 +0200

    視認性のため、canvasに枠線をつけた

--- frontend/src/features/game/Game.tsx
+++ frontend/src/features/game/Game.tsx
@@ -1,6 +1,13 @@
 'use client';
+import { useEffect } from 'react';
 
 const Game = () => {
+  useEffect(() => {
+    const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    canvas.style.border = '4px solid';
+    canvas.style.color = 'black';
+  }, []);
+
   return <canvas width={400} height={400} id={'canvas'}></canvas>;
 };
 
