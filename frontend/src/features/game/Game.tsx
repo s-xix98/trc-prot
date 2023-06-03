11fcbcce3efa90c4e8eca321b766aa4f2e7f7d6a
nori <nori623p@gmail.com>
2023-06-03 07:23:43 +0200

    玉が動くようにした

--- frontend/src/features/game/Game.tsx
+++ frontend/src/features/game/Game.tsx
@@ -1,6 +1,8 @@
 'use client';
 import { useEffect, useState } from 'react';
 
+import { useInterval } from '@/hooks/useInterval';
+
 const DrawBall = (
   ctx: CanvasRenderingContext2D,
   x: number,
@@ -18,6 +20,9 @@ const Game = () => {
   const width = 400;
   const height = 400;
   const canvasId = 'canvas';
+  let ballX = width / 2;
+  let ballY = height / 2;
+
   const [ctx, setContext] = useState<CanvasRenderingContext2D | null>(null);
 
   useEffect(() => {
@@ -28,12 +33,14 @@ const Game = () => {
     setContext(canvasContext);
   }, []);
 
-  useEffect(() => {
+  useInterval(() => {
     if (ctx === null) {
       return;
     }
-    DrawBall(ctx, width / 2, height / 2, 10);
-  });
+    DrawBall(ctx, ballX, ballY, 10);
+    ballX += 1;
+    ballY += 1;
+  }, 10);
 
   return <canvas width={width} height={height} id={canvasId}></canvas>;
 };
