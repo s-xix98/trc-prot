385bb8d9dab82e0a554049c1853b31b544520af2
wyoheiii <seikimatu4649@gmail.com>
2023-06-04 01:45:41 +0900

    nextauthにaccessTokenを入れる型がないので定義してあげた

---
+++ frontend/src/app/types/next-auth.d.ts
@@ -0,0 +1,21 @@
+// module拡張するにはmoduleを呼び出さないといけないっぽい
+// eslint-disable-next-line
+import { JWT } from 'next-auth/jwt';
+// eslint-disable-next-line
+import NextAuth from 'next-auth';
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    accessToken?: string;
+  }
+}
+
+declare module "next-auth" {
+  interface Session {
+    accessToken?: string;
+  }
+
+  interface User {
+    accessToken?: string;
+  }
+}
