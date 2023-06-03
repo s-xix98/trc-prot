6e0da31e984422c611af7bf0ecfd1eff2d078616
nori <nori623p@gmail.com>
2023-06-03 19:19:21 +0200

    Revert "スクショの出力が安定しないので一旦gameは無視"
    This reverts commit 98caf2c243588cc813a30b95eb7afa49701519c3.

--- frontend/scripts/check_storybook_file.sh
+++ frontend/scripts/check_storybook_file.sh
@@ -1,5 +1,5 @@
 set -e
 
-for file in $(find src -type f -name '*.tsx' ! -name '*.stories.tsx' ! -path 'src/features/game/Game.tsx' ! -name './src/lib/*'  ! -path 'src/lib/*' | grep -v -e 'src/stories' -e 'main\.tsx'); do
+for file in $(find src -type f -name '*.tsx' ! -name '*.stories.tsx' ! -name './src/lib/*' ! -path 'src/lib/*'| grep -v -e 'src/stories' -e 'main\.tsx'); do
 	echo ${file} && test -f ${file%.tsx}.stories.tsx
 done
--- frontend/scripts/create_storybook_file.sh
+++ frontend/scripts/create_storybook_file.sh
@@ -1,5 +1,5 @@
 set -e
 
-for file in $(find src -type f -name '*.tsx' ! -name '*.stories.tsx' ! -path 'src/features/game/Game.tsx' ! -path 'src/lib/*' | grep -v -e 'src/stories' -e 'main\.tsx'); do
+for file in $(find src -type f -name '*.tsx' ! -name '*.stories.tsx' ! -path 'src/lib/*' | grep -v -e 'src/stories' -e 'main\.tsx'); do
 	touch ${file%.tsx}.stories.tsx
 done
