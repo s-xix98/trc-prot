9e759dc6681bcefed326aa069610180058e5b106
wyoheiii <seikimatu4649@gmail.com>
2023-06-06 04:37:17 +0900

    test fileの依存関係を解決した

--- backend/src/auth/auth.controller.spec.ts
+++ backend/src/auth/auth.controller.spec.ts
@@ -1,16 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { JwtService } from '@nestjs/jwt';
 
 import { TestModule } from '../test/test.module';
 
 import { AuthController } from './auth.controller';
-
+import { AuthModule } from './auth.module';
+import { AuthService } from './auth.service';
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [TestModule],
+      imports: [TestModule, AuthModule],
       controllers: [AuthController],
+      providers: [AuthService, JwtService],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -1,16 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { JwtService } from '@nestjs/jwt';
 
 import { TestModule } from '../test/test.module';
 
 import { AuthService } from './auth.service';
+import { AuthModule } from './auth.module';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [TestModule],
-      providers: [AuthService],
+      imports: [TestModule, AuthModule],
+      providers: [AuthService, JwtService],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
