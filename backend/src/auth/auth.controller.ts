b6f366e8ba61929412babd2f50f9be5edb510b26
wyoheiii <seikimatu4649@gmail.com>
2023-06-05 16:31:08 +0900

    ftLoginエンドポイントを作った

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService :AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('42login')
+  async ftLogin() {
+    return this.authService.ftLogin();
+  }
+}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  ftLogin() {
+    const mockJwt = 'mockJwt';
+    return mockJwt;
+  }
+}
