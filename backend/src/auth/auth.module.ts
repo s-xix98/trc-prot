564fb1caaac4c1b809fdee8adb0ec9fd8be74edd
wyoheiii <seikimatu4649@gmail.com>
2023-06-06 02:01:43 +0900

    login時にアカウントを作成するようにした

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -4,8 +4,10 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import {JwtModule} from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PrismaModule } from '../prisma/prisma.module';
 @Module({
   imports: [
+    PrismaModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,17 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { accessToken } from './types/auth.types';
 import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../prisma/prisma.service';
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(private readonly jwtService: JwtService, private readonly prismaService:PrismaService) {}
 
   async ftLogin(): Promise<accessToken> {
-    const mockUserData = {
-      id: 'mockId',
-      username: 'mockUsername',
+
+    const mockAuthLoginDto = {
+      email: 'mockEmail@example.com',
+      mockUsername: 'mockUsername',
+      mockHashedPassword: 'mockHashedPassword',
+      providerId: 'mockSub',
+      providerName: '42',
     };
 
-    return { jwt: await this.generateJwt(mockUserData.id, mockUserData.username)};
+    const createdUser = await this.prismaService.user.create({
+      include: { auth: true },
+      data: {
+        email: mockAuthLoginDto.email,
+        username: mockAuthLoginDto.mockUsername,
+        // TODO hashedPassword? にしたら消す
+        hashedPassword: mockAuthLoginDto.mockHashedPassword,
+        auth: {
+          create: {
+            providerId: mockAuthLoginDto.providerId,
+            providerName: mockAuthLoginDto.providerName,
+          },
+        },
+      },
+    });
+    return { jwt: await this.generateJwt(createdUser.id, createdUser.username)};
   }
 
   async generateJwt(id: string, username: string): Promise<string> {
