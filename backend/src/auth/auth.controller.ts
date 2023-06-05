f6154fdf99295f62a11180b0fca5cda0290610b5
wyoheiii <seikimatu4649@gmail.com>
2023-06-06 03:04:00 +0900

    swaggerで確認できるようにした

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -2,12 +2,15 @@ import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { accessToken } from './types/auth.types';
 import { authLoginDto } from './dto/auth.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 @Controller('auth')
+@ApiTags('/auth')
 export class AuthController {
   constructor(private readonly authService :AuthService) {}
 
   @HttpCode(HttpStatus.OK)
   @Post('42login')
+  @ApiOperation({ summary: '42login' })
   async ftLogin(@Body() dto: authLoginDto):Promise<accessToken> {
     return this.authService.ftLogin(dto);
   }
--- backend/src/auth/dto/auth.dto.ts
+++ backend/src/auth/dto/auth.dto.ts
@@ -1,5 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
 export class authLoginDto {
+  @ApiProperty({ example: 'hoge@example.com', description: 'test' })
   email: string;
+  @ApiProperty({ example: 'ft12345', description: 'test' })
   providerId: string;
+  @ApiProperty({ example: '42', description: 'test' })
   providerName: string;
 }
