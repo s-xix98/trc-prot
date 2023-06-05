f1268b620ba7a2b71e715d82b3cbc03a34170366
wyoheiii <seikimatu4649@gmail.com>
2023-06-05 16:31:08 +0900

    providerLoginエンドポイントを作った

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('providerLogin')
+  async providerLogin() {
+    return this.authService.providerLogin();
+  }
+}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+  providerLogin() {
+    const mockJwt = 'mockJwt';
+    return mockJwt;
+  }
+}
