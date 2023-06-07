67c1abb26ce9aaba59d5dbbce2babe6e9ca0467c
wyoheiii <seikimatu4649@gmail.com>
2023-06-07 23:06:31 +0900

    mock userIdとmock usernameからjwtを作って返すようにした

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@nestjs/common';
 
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
 
-  providerLogin() {
-    const mockJwt = 'mockJwt';
-    return mockJwt;
+  async providerLogin(): Promise<string> {
+    const mockUserData = {
+      id: 'mockId',
+      username: 'mockUsername',
+    };
+
+    const jwt = await this.generateJwt(mockUserData.id, mockUserData.username);
+    return jwt;
+  }
+
+  async generateJwt(userId: string, username: string): Promise<string>{
+    const payload = {
+      userId,
+      username,
+    };
+
+    return this.jwtService.signAsync(payload, { expiresIn: '1h' });
   }
 }
