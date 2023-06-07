b0ef4057238bbb8066fd88bf7fb29a5ac9c58c0b
wyoheiii <seikimatu4649@gmail.com>
2023-06-07 23:24:16 +0900

    swaggerでproviderLoginのレスポンスを確認できるようにした

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { accessToken } from './types/auth.types';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+
 @Controller('auth')
+@ApiTags('/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @HttpCode(HttpStatus.OK)
   @Post('providerLogin')
+  @ApiOperation({ summary: 'providerLogin' })
   async providerLogin(): Promise<accessToken> {
     return this.authService.providerLogin();
   }
