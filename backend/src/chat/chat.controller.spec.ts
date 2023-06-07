7bb725d6e8e9f6bf118a9fe542dc9fee16867ee0
wyoheiii <seikimatu4649@gmail.com>
2023-06-07 22:19:14 +0900

    test fileでDIの依存関係でエラーが出るのを解消した

--- backend/src/chat/chat.controller.spec.ts
+++ backend/src/chat/chat.controller.spec.ts
@@ -1,13 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { ChatController } from './chat.controller';
+import { ChatService } from './chat.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { TestModule } from '../test/test.module';
 
 describe('ChatController', () => {
   let controller: ChatController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TestModule],
       controllers: [ChatController],
+      providers:[ChatService],
     }).compile();
 
     controller = module.get<ChatController>(ChatController);
--- backend/src/chat/chat.service.spec.ts
+++ backend/src/chat/chat.service.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { ChatService } from './chat.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { TestModule } from '../test/test.module';
 
 describe('ChatService', () => {
   let service: ChatService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TestModule],
       providers: [ChatService],
     }).compile();
 
