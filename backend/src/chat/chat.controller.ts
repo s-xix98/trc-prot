e268e6a146a3ae3cb9406cf278450f4aa963d6b0
wyoheiii <seikimatu4649@gmail.com>
2023-06-07 21:59:48 +0900

    swaggerでgetChannelsのレスポンスを確認できるようにした

--- backend/src/chat/chat.controller.ts
+++ backend/src/chat/chat.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatRoom } from '@prisma/client';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 @Controller('chat')
+@ApiTags('/chat')
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
 
   @Get('rooms')
+  @ApiOperation({ summary: 'getChannels' })
   async getAllChannels(): Promise<ChatRoom[]> {
     return this.chatService.getAllChannels();
   }
