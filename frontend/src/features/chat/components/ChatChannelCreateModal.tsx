e5a30ef70bb948fcdf19800537122933a3f1ac85
wyoheiii <seikimatu4649@gmail.com>
2023-06-08 19:49:04 +0900

    クリックしたらモーダルが開かれるようにした

--- frontend/src/features/chat/components/ChatChannelCreateModal.tsx
+++ frontend/src/features/chat/components/ChatChannelCreateModal.tsx
@@ -1,4 +1,25 @@
+'use client';
+import Modal from 'react-modal';
+import { useState } from 'react';
+
+Modal.setAppElement('body');
 
 export const ChatChannelCreateModal = () => {
-  return <div>ChatChannelCreateModal</div>;
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+  const onClick = () => {
+    setModalIsOpen(true);
+  };
+  return (
+    <div>
+      <div onClick={onClick}>ChannelCreate</div>
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}>
+        </Modal>
+    </div>
+  );
 }
