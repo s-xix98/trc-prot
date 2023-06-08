2c4faee2a729d4f51776c071196d10bbe2d6cd0d
wyoheiii <seikimatu4649@gmail.com>
2023-06-08 20:08:02 +0900

    modalのサイズ、位置を変更した

--- frontend/src/features/chat/components/ChatChannelCreateModal.tsx
+++ frontend/src/features/chat/components/ChatChannelCreateModal.tsx
@@ -4,6 +4,19 @@ import { useState } from 'react';
 
 Modal.setAppElement('body');
 
+const customStyles = {
+  content: {
+    width: '200px',
+    height: '250px',
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  }
+};
+
 export const ChatChannelCreateModal = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
@@ -18,7 +31,8 @@ export const ChatChannelCreateModal = () => {
       <div onClick={onClick}>ChannelCreate</div>
         <Modal
           isOpen={modalIsOpen}
-          onRequestClose={closeModal}>
+          onRequestClose={closeModal}
+          style={customStyles}>
         </Modal>
     </div>
   );
