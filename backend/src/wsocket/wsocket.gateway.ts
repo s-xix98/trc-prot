8625924d0551d8462f90acea358544d3e0990ced
wyoheiii <seikimatu4649@gmail.com>
2023-06-12 01:48:03 +0900

    prefixをつけて名前空間に入れる関数を作った

--- backend/src/wsocket/wsocket.gateway.ts
+++ backend/src/wsocket/wsocket.gateway.ts
@@ -11,5 +11,8 @@ export class WsocketGateway {
   @WebSocketServer()
   server: Server;
 
+  socketJoinNamespace(client: Socket, id: string) {
+    const prefixedId = 'chat' + id;
+    client.join(prefixedId);
   }
 }
