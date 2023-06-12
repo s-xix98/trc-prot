13bd617a719df9b7e722e47ee094569d355872cd
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-06-13 02:26:02 +0900

    height width を可変にした

--- frontend/src/components/Elements/Modal/ModalView.tsx
+++ frontend/src/components/Elements/Modal/ModalView.tsx
@@ -7,18 +7,22 @@ export const ModalView = ({
   children,
   modalIsOpen,
   closeModal,
+  height = '90%',
+  width = '90%',
 }: {
   children: ReactNode;
   modalIsOpen: boolean;
   closeModal: () => void;
+  height?: string;
+  width?: string;
 }) => {
   const style = {
     position: 'absolute' as const,
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    height: '90%',
-    width: '90%',
+    height: height,
+    width: width,
     borderRadius: '5px',
     padding: '10px',
     backgroundColor: '#252525',
