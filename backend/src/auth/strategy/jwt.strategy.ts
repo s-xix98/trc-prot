769efea3a3b16dbe96250140ed78fa997ad05c2d
wyoheiii <seikimatu4649@gmail.com>
2023-06-15 04:00:17 +0900

    github actionsにSECRETが設定されてないから、ない場合は適当な文字列で代用

--- backend/src/auth/strategy/jwt.strategy.ts
+++ backend/src/auth/strategy/jwt.strategy.ts
@@ -14,7 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get('JWT_SECRET'),
+      secretOrKey: configService.get('JWT_SECRET') || 'hoge',
     });
   }
 
