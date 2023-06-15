ad3a0e49bae5ee211c7e4a9d913721c06e725a10
wyoheiii <seikimatu4649@gmail.com>
2023-06-15 19:42:09 +0900

    mockユーザーをリストで表示

--- frontend/src/features/user/components/Search.tsx
+++ frontend/src/features/user/components/Search.tsx
@@ -1,8 +1,20 @@
 
 export const SearchUser = () => {
+  const mockUsers = []
+  for (let i = 0; i < 10; i++) {
+    mockUsers.push(`user${i}`);
+  }
+
   return (
     <>
-      <div>Search User</div>
+      <div>User Search</div>
+      <div>
+      {mockUsers.map((user, key) => {
+        return (
+          <li key={key}> {user}</li>
+        )
+        })};
+      </div>
     </>
   );
 };
