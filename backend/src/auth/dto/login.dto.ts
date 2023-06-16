48fe7a27aaa7846359d2577a9fddca6f619392ad
wyoheiii <seikimatu4649@gmail.com>
2023-06-17 01:01:08 +0900

    試しにemail loginにvalidationを設置

--- backend/src/auth/dto/login.dto.ts
+++ backend/src/auth/dto/login.dto.ts
@@ -1,8 +1,15 @@
+import {IsNotEmpty, MinLength , IsString, IsEmail} from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class loginDto {
   @ApiProperty({ example: 'nori@example.com', description: 'test' })
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
   @ApiProperty({ example: 'norinori', description: 'test' })
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   hashedPassword: string;
 }
