ebddac22623ca3b039dfede8f3e488986c232d6e
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-06-16 12:37:51 +0900

    add sb file

---
+++ frontend/src/app/login/page.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Login from './page';
+
+const meta = {
+  component: Login,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Login>;
+
+export default meta;
+
+type Story = StoryObj<typeof Login>;
+
+export const Basic: Story = {};
