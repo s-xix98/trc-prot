83519525f8117752e8ac6feba8b3f92f1801e6f2
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-06-16 12:25:06 +0900

    uiがあまりにも酷いのでちょい改善

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -4,14 +4,16 @@ import { MainLayout } from '@/components/Layout/MainLayout';
 import { User } from '@/features/user/components/User';
 import { useRedirectToHome } from '@/hooks/useLogin';
 
+// TODO : UI 改善する
 export default function Login() {
   useRedirectToHome();
 
   return (
     <MainLayout>
-      <h1>Login</h1>
       <User>
-        <p>none</p>
+        <div style={{ margin: 'auto' }}>
+          <h1>Please login</h1>
+        </div>
       </User>
     </MainLayout>
   );
