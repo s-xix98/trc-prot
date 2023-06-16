71fbad470cf44cde40a5470f295289eb1f91906c
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-06-16 11:34:28 +0900

    虚無のloginページを作成

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { MainLayout } from '@/components/Layout/MainLayout';
+
+export default function Login() {
+  return (
+    <MainLayout>
+      <h1>Login</h1>
+    </MainLayout>
+  );
+}
