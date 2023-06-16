0642e7201b9a325987c82a76385b17d29e473c3c
wyoheiii <seikimatu4649@gmail.com>
2023-06-16 21:51:05 +0900

    fmt

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,11 +1,11 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { User } from '@prisma/client';
 
 import { AuthService } from './auth.service';
 import { accessToken } from './types/auth.types';
 import { signUpDto } from './dto/signUp.dto';
 import { loginDto } from './dto/login.dto';
-import { User } from '@prisma/client';
 
 @Controller('auth')
 @ApiTags('/auth')
@@ -23,7 +23,6 @@ export class AuthController {
   @ApiOperation({ summary: 'signUp nori' })
   async signUp(@Body() dto: signUpDto): Promise<User> {
     return this.authService.signUp(dto);
-
   }
 
   @HttpCode(HttpStatus.OK)
