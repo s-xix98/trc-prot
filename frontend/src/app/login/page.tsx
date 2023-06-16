f43bf52aea8bff81bfbac4b22f6398e14c46646e
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-06-16 12:02:55 +0900

    適当にlogin用のボタンとか表示するため <User> を表示

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,11 +1,15 @@
 'use client';
 
 import { MainLayout } from '@/components/Layout/MainLayout';
+import { User } from '@/features/user/components/User';
 
 export default function Login() {
   return (
     <MainLayout>
       <h1>Login</h1>
+      <User>
+        <p>none</p>
+      </User>
     </MainLayout>
   );
 }
