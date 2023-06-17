90eaf43ffbcd8fe3a72cb75e46cfc8382710234f
wyoheiii <seikimatu4649@gmail.com>
2023-06-17 16:22:43 +0900

    strategy secretに合わせてjwt module secretの文字列を設定した

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -17,7 +17,7 @@ import { JwtStrategy } from './strategy/jwt.strategy';
       inject: [ConfigService],
 
       useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET') || '',
+        secret: configService.get<string>('JWT_SECRET') || 'hoge',
       }),
     }),
   ],
