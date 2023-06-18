bf62c0395b7fa00d534167bb8088391807d9cfc2
nori <nori623p@gmail.com>
2023-06-18 08:35:51 +0200

    フロントを実装 ボタンを押すとマッチングにinQして、マッチすると相手の名前が表示される予定

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -5,5 +5,10 @@ export class GameGateway {
   handleConnection() {
     console.log('game connection');
   }
+  // stub
+  @SubscribeMessage('matchmake')
+  matchmake(client: Socket, user: UserInfo) {
+    console.log('mathmake')
+  }
   }
 }
---
+++ frontend/src/features/game/Matching.tsx
@@ -0,0 +1,28 @@
+import { useSocket } from '@/hooks/useSocket';
+import { useState } from 'react';
+import { socket } from '@/socket';
+import { useCurrentUser } from '@/hooks/useCurrentUser';
+
+export const Matching = () => {
+  const [isMatched, setMatched] = useState(false);
+  const [name, setName] = useState('');
+  const { userInfo } = useCurrentUser();
+
+  useSocket('matched', (side: number, enemyName: string) => {
+    console.log(side, enemyName);
+    setName(enemyName);
+    setMatched(true);
+  });
+
+  const onClickAct = () => {
+    socket.emit('matchmake', userInfo);
+  };
+  // console.log("ui", userInfo);
+  return (
+    <>
+      <button onClick={onClickAct}>button</button>
+      {isMatched && <h1> {name} </h1>}
+      {!isMatched && <h1> WAITING... </h1>}
+    </>
+  );
+};
