627d7902c9a3db48fe15828d6f2ded0346f176a7
wyoheiii <seikimatu4649@gmail.com>
2023-06-18 20:24:44 +0900

    handleMessageの型をbackendから送られてくるデータに合わせた

--- frontend/src/features/chat/types/MessageDto.ts
+++ frontend/src/features/chat/types/MessageDto.ts
@@ -1,3 +1,4 @@
+import { UserInfo } from '../../user/types/UserDto';
 // バックエンドの方と同じデータ構造
 
 export type MessageDto = {
@@ -6,8 +7,8 @@ export type MessageDto = {
 };
 
 export type handleMessageDto = {
-  username: string;
-  msg: string;
+  content: string;
+  user:UserInfo;
 };
 
 export type sendMessageDto = {
