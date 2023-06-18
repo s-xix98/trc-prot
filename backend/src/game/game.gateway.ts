b1f74f0a5110c806ae1e4ff13f360f6b7c1d0910
nori <nori623p@gmail.com>
2023-06-18 08:13:30 +0200

    一方が切断したらマッチング状態を解除するようにした 一方がdisconnectしたときにもう一方に通知し。マッチングを削除したい handledisconnectの入力がsocketなので、マッチングを特定するためにsocket.id -> user.idのマップが必要 対戦相手のsocketを特定するためにuser.id -> socketのマップが必要

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -13,10 +13,41 @@ type PlayerData = { client: Socket; data: UserInfo };
 @WebSocketGateway()
 export class GameGateway {
   private matchedUsers = new Map<string, UserInfo>();
+  private userSockMap = new Map<string, Socket>();
+  private sockUserMap = new Map<string, string>();
   private waitingUser: PlayerData | undefined = undefined;
 
   handleConnection() {
     console.log('game connection');
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log('game handleDisconnect');
+    const selfUserId = this.sockUserMap.get(client.id);
+    if (selfUserId === undefined) {
+      return;
+    }
+    const enemyUser = this.matchedUsers.get(selfUserId);
+    if (enemyUser === undefined) {
+      // NotReach
+      return;
+    }
+    const enemySocket = this.userSockMap.get(enemyUser.id);
+    if (enemySocket === undefined) {
+      // NotReach
+      return;
+    }
+    enemySocket.emit('enemy disconnected');
+
+    console.log('delete match data');
+    this.matchedUsers.delete(selfUserId);
+    this.matchedUsers.delete(enemyUser.id);
+
+    this.userSockMap.delete(selfUserId);
+    this.userSockMap.delete(enemyUser.id);
+
+    this.sockUserMap.delete(client.id);
+    this.sockUserMap.delete(enemySocket.id);
   }
 
   @SubscribeMessage('matchmake')
@@ -33,6 +64,12 @@ export class GameGateway {
     }
     this.matchedUsers.set(this.waitingUser.data.id, user);
     this.matchedUsers.set(user.id, this.waitingUser.data);
+
+    this.userSockMap.set(this.waitingUser.data.id, this.waitingUser.client);
+    this.sockUserMap.set(this.waitingUser.client.id, this.waitingUser.data.id);
+
+    this.userSockMap.set(user.id, client);
+    this.sockUserMap.set(client.id, user.id);
 
     this.waitingUser.client.emit('matched', PlaySide.LEFT, user.username);
     client.emit('matched', PlaySide.RIGHT, this.waitingUser.data.username);
