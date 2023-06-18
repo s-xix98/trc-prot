9d5cd09a36e77601cbc984320ed73ed8327709e1
wyoheiii <seikimatu4649@gmail.com>
2023-06-18 22:49:46 +0900

    fmt

--- frontend/src/features/chat/components/ChatTalkArea.tsx
+++ frontend/src/features/chat/components/ChatTalkArea.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import { Container } from '@/components/Layout/Container';
 import { useSocket } from '@/hooks/useSocket';
 import { useScroll } from '@/hooks/useScroll';
+import { BACKEND } from '@/constants';
 
 import { handleMessageDto } from '../types/MessageDto';
 import { chatChannelDto } from '../types/chatChannelDto';
@@ -10,8 +12,6 @@ import { chatChannelDto } from '../types/chatChannelDto';
 import { ChatHistory } from './ChatHistory';
 import { ChatInput } from './ChatInput';
 import { ChatTalkAreaHeader } from './ChatTalkAreaHeader';
-import axios from 'axios';
-import { BACKEND } from '@/constants';
 
 export const ChatTalkArea = ({
   selectedChannel,
@@ -22,16 +22,15 @@ export const ChatTalkArea = ({
 
   const { scrollBottomRef, handleScroll } = useScroll(chatHistMsgs);
 
-  useEffect(()=> {
-
-    axios.get(BACKEND + '/chat/rooms/' + selectedChannel.id + '/history')
-    .then( (res)=> {
-      setChatHistMsgs(res.data);
-    })
-    .catch(()=>{
-      console.log('room 取得失敗');
-    });
-
+  useEffect(() => {
+    axios
+      .get(BACKEND + '/chat/rooms/' + selectedChannel.id + '/history')
+      .then((res) => {
+        setChatHistMsgs(res.data);
+      })
+      .catch(() => {
+        console.log('room 取得失敗');
+      });
   }, [selectedChannel]);
 
   const onMessage = (data: handleMessageDto[]) => {
