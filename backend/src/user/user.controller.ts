ac99a5274a39bed0491bd072c1fff881206ecb65
wyoheiii <seikimatu4649@gmail.com>
2023-06-19 19:45:47 +0900

    自身の情報を取得するエンドポイントを作成した 値はmock

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Request, UseGuards } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiParam } from '@nestjs/swagger';
 
 import { UserService } from './user.service';
+import { JwtAuthGuard } from '../auth/guard/jwt-auth.gurad';
 
 @Controller('user')
 @ApiTags('/user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  findMe(@Request() req: any) {
+    return req.user;
+  }
 
   @ApiOperation({ summary: 'findOne' })
   @ApiParam({
