e51016ec1e17717aca09bb2182ccee950443d6e4
wyoheiii <seikimatu4649@gmail.com>
2023-06-21 15:23:14 +0900

    signUPの方もhook化した

--- frontend/src/features/user/api/userSignUp.ts
+++ frontend/src/features/user/api/userSignUp.ts
@@ -1,8 +1,16 @@
 import axios, { AxiosResponse } from 'axios';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context';
 
-import { SignUpDto, UserInfo } from '../types/UserDto';
+import { accessToken } from '@/app/login/types/accessToken';
+import { tokenStorage } from '@/utils/tokenStorage';
+
+import { SignUpDto } from '../types/UserDto';
 
 import { BACKEND } from '../../../constants';
+import { useSessionAxios } from '../../hooks/useSessionAxios';
+import { UserInfo } from '../types/UserDto';
+import { useSetAtom } from 'jotai';
+import { userInfoAtom } from '@/stores/jotai';
 
 export const userSignUp = async (
   username: string,
@@ -32,3 +40,27 @@ export const userSignUp = async (
       console.log('signup err:', err);
     });
 };
+
+export const useSignUp = () => {
+  const setUserInfo = useSetAtom(userInfoAtom);
+  const sessionAxios = useSessionAxios();
+
+  const signUp = async (username: string,email: string, pass: string) => {
+
+    const loginDto: SignUpDto = {username:username , email: email, hashedPassword: pass };
+
+    try {
+      const res = await axios.post<accessToken>(BACKEND + '/auth/signup', loginDto);
+      tokenStorage.set(res.data.jwt);
+
+      const user = await sessionAxios.get<UserInfo>('/user/me');
+      setUserInfo(user.data);
+      // TODO とりあえずlogだけ
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return signUp;
+};
+
