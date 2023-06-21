b1ced3f7262fc735579acd6a445b298ed93d7c4a
wyoheiii <seikimatu4649@gmail.com>
2023-06-21 20:54:31 +0900

    fmt

--- frontend/src/features/user/components/Search.tsx
+++ frontend/src/features/user/components/Search.tsx
@@ -1,20 +1,20 @@
 import { ChangeEvent, useState } from 'react';
+import styled from 'styled-components';
 
 import { ContainerItem } from '@/components/Layout/ContainerItem';
 import { Container } from '@/components/Layout/Container';
 import { Input } from '@/components/Elements/Input/Input';
 import { useFocus } from '@/hooks/useFocus';
+import { isUserInfo, isChatChannelDto } from '@/utils/typeGuars';
 
 import { useSearch } from '../api/useSearch';
 import { UserInfo } from '../types/UserDto';
-import { chatChannelDto } from '../../chat/types/chatChannelDto';
-import styled from 'styled-components';
-import { isUserInfo, isChatChannelDto } from '@/utils/typeGuars';
 
+import { chatChannelDto } from '../../chat/types/chatChannelDto';
 
 const StyledButton = styled.button`
   align-items: flex-start;
-`
+`;
 
 export const SearchUserOrChannel = () => {
   const { focusRef } = useFocus();
@@ -22,7 +22,7 @@ export const SearchUserOrChannel = () => {
   const [isSearchingUser, setIsSearchingUser] = useState(true);
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if(isSearchingUser){
+    if (isSearchingUser) {
       searcher('/user/search/', e.target.value);
     } else {
       searcher('/chat/search/', e.target.value);
@@ -33,22 +33,27 @@ export const SearchUserOrChannel = () => {
     <div>
       <Container flexDirection={'column'}>
         <ContainerItem>
-        <StyledButton onClick={() => setIsSearchingUser(true)}>
-        User Search
-        </StyledButton>
-        <StyledButton onClick={() => setIsSearchingUser(false)}>
-        room Search
-        </StyledButton>
-        <ContainerItem overflowY="scroll">
-          {searchedList.map((elm, key) => {
-            if (isUserInfo(elm)){
-              return <p key={key}> {elm.username}</p>
-            } else if (isChatChannelDto(elm)){
-              return <p key={key}> {elm.roomName}</p>
-            }
-          })}
+          <StyledButton onClick={() => setIsSearchingUser(true)}>
+            User Search
+          </StyledButton>
+          <StyledButton onClick={() => setIsSearchingUser(false)}>
+            room Search
+          </StyledButton>
+          <ContainerItem overflowY="scroll">
+            {searchedList.map((elm, key) => {
+              if (isUserInfo(elm)) {
+                return <p key={key}> {elm.username}</p>;
+              } else if (isChatChannelDto(elm)) {
+                return <p key={key}> {elm.roomName}</p>;
+              }
+            })}
+          </ContainerItem>
         </ContainerItem>
-        <Input focusRef={focusRef} onChangeAct={onChange} placeholder= {isSearchingUser? "username": "roomname"}  />
+        <Input
+          focusRef={focusRef}
+          onChangeAct={onChange}
+          placeholder={isSearchingUser ? 'username' : 'roomname'}
+        />
       </Container>
     </div>
   );
--- frontend/src/utils/typeGuars.ts
+++ frontend/src/utils/typeGuars.ts
@@ -1,14 +1,12 @@
-import {chatChannelDto} from "../features/chat/types/chatChannelDto";
-import { UserInfo } from "../features/user/types/UserDto";
+import { chatChannelDto } from '../features/chat/types/chatChannelDto';
+import { UserInfo } from '../features/user/types/UserDto';
 
 //eslint-disable-next-line
 export const isChatChannelDto = (obj: any): obj is chatChannelDto => {
   return obj && typeof obj.roomName === 'string';
-}
+};
 
 //eslint-disable-next-line
 export const isUserInfo = (obj: any): obj is UserInfo => {
   return obj && typeof obj.username === 'string';
-}
-
-
+};
