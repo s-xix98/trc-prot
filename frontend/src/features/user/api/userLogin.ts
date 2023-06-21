965823603e62c908f542d8fbcdf2fc34d29f5e1d
wyoheiii <seikimatu4649@gmail.com>
2023-06-21 15:03:25 +0900

    localStorageにtokenをセットし,user情報を取得しatomにセットするhook作成

--- frontend/src/features/user/api/userLogin.ts
+++ frontend/src/features/user/api/userLogin.ts
@@ -3,6 +3,11 @@ import axios, { AxiosResponse } from 'axios';
 import { LoginDto, UserInfo } from '../types/UserDto';
 
 import { BACKEND } from '../../../constants';
+import { useSetAtom } from 'jotai';
+import { tokenStorage } from '@/utils/tokenStorage';
+import { accessToken } from '@/app/login/types/accessToken';
+import { useSessionAxios } from '@/hooks/useSessionAxios';
+import { userInfoAtom } from '@/stores/jotai';
 
 export const userLogin = async (
   email: string,
@@ -24,4 +29,27 @@ export const userLogin = async (
       // TODO とりあえずなにもしない
       console.log('login err:', err);
     });
+};
+
+
+export const useLogin = () => {
+  const setUserInfo = useSetAtom(userInfoAtom);
+  const sessionAxios = useSessionAxios();
+
+  const login = async (email: string, pass: string) => {
+    const loginDto: LoginDto = { email: email, hashedPassword: pass };
+
+    try {
+      const res = await axios.post<accessToken>(BACKEND + '/auth/login', loginDto);
+      tokenStorage.set(res.data.jwt);
+
+      const user = await sessionAxios.get<UserInfo>('/user/me');
+      setUserInfo(user.data);
+    // TODO とりあえずlogだけ
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return login;
 };
