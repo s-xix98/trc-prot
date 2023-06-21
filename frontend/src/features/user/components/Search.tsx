7de6ab11299d8e07780cd2579c80e3f447c7e697
wyoheiii <seikimatu4649@gmail.com>
2023-06-21 17:48:24 +0900

    fmt

--- frontend/src/features/user/components/Search.tsx
+++ frontend/src/features/user/components/Search.tsx
@@ -6,27 +6,27 @@ import { Container } from '@/components/Layout/Container';
 import { Input } from '@/components/Elements/Input/Input';
 
 import { UserInfo } from '../types/UserDto';
+
 import { useSessionAxios } from '../../../hooks/useSessionAxios';
 
 export const SearchUser = () => {
   const [searchUsers, setSearchUsers] = useState<string[]>([]);
   const axios = useSessionAxios();
 
-
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-
     if (e.target.value.length === 0) {
       setSearchUsers([]);
       return;
     }
 
-    axios.get<UserInfo[]>('/user/search/' + e.target.value)
-    .then((res)=>{
-      const users = res.data.map((user: UserInfo) => user.username);
-      setSearchUsers(users);
-    })
-    // TODO とりあえず何もしない エラー表示とか出したいね
-    .catch((err)=> console.log(err));
+    axios
+      .get<UserInfo[]>('/user/search/' + e.target.value)
+      .then((res) => {
+        const users = res.data.map((user: UserInfo) => user.username);
+        setSearchUsers(users);
+      })
+      // TODO とりあえず何もしない エラー表示とか出したいね
+      .catch((err) => console.log(err));
   };
 
   return (
