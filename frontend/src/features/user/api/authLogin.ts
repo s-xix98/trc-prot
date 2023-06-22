3c8f352f916a1619b906b7348b3a57266d4ebef9
wyoheiii <seikimatu4649@gmail.com>
2023-06-22 20:03:24 +0900

    backendから自身の情報を取得する処理を追加

--- frontend/src/features/user/api/authLogin.ts
+++ frontend/src/features/user/api/authLogin.ts
@@ -1,7 +1,14 @@
+import { useSessionAxios } from "@/hooks/useSessionAxios";
 import { tokenStorage } from "@/utils/tokenStorage";
 import { BACKEND } from "@/constants";
+import { UserInfo } from "../types/UserDto";
+import { useSetAtom } from "jotai";
+import { userInfoAtom } from "@/stores/jotai";
 import { useEffect } from "react";
+
 export const useAuthLogin = () => {
+  const axios = useSessionAxios();
+  const setUserInfo = useSetAtom(userInfoAtom);
 
   useEffect(() => {
     const queryParam = new URLSearchParams(window.location.search);
@@ -13,7 +20,16 @@ export const useAuthLogin = () => {
 
     tokenStorage.set(token);
 
-  }, [setUserInfo]);
+    axios.get<UserInfo>('/user/me')
+    .then((res) => {
+      console.log(res.data);
+      setUserInfo(res.data);
+      // TODO とりあえずlogだけ
+      }).catch((err) => {
+        console.log(err);
+    });
+
+  }, [axios, setUserInfo]);
 
   const login = (path: string) => {
     window.location.href = BACKEND + path;
