1168174f49adc231b951b46b6c29ee92aaceb7c5
wyoheiii <seikimatu4649@gmail.com>
2023-06-22 19:47:25 +0900

    クエリパラメーターからtokenをlocalStorageにセットする

--- frontend/src/features/user/api/authLogin.ts
+++ frontend/src/features/user/api/authLogin.ts
@@ -1,5 +1,19 @@
+import { tokenStorage } from "@/utils/tokenStorage";
 import { BACKEND } from "@/constants";
+import { useEffect } from "react";
 export const useAuthLogin = () => {
+
+  useEffect(() => {
+    const queryParam = new URLSearchParams(window.location.search);
+    const token = queryParam.get('access_token');
+
+    if (!token) {
+      return;
+    }
+
+    tokenStorage.set(token);
+
+  }, [setUserInfo]);
 
   const login = (path: string) => {
     window.location.href = BACKEND + path;
