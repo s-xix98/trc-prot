e9507d31eb09665d613e8f39317dfe34c199eae1
wyoheiii <seikimatu4649@gmail.com>
2023-06-23 18:05:51 +0900

    modalを設置, 中身はスタブ

--- frontend/src/features/chat/components/ChannelEntryModal.tsx
+++ frontend/src/features/chat/components/ChannelEntryModal.tsx
@@ -1,4 +1,7 @@
+import { useModal } from "@/hooks/useModal";
 import { chatChannelDto } from "../types/chatChannelDto";
+import { ModalView } from "@/components/Elements/Modal/ModalView";
+import { Container } from "@mui/material";
 
 export const ChannelEntryModal = ({
   channelData,
@@ -7,6 +10,17 @@ export const ChannelEntryModal = ({
   channelData:chatChannelDto
   key:number
 }) => {
+  const {modalIsOpen, openModal, closeModal} = useModal();
 
-  return <p key={key}>{channelData.roomName}</p>;
+  return (
+    <Container>
+      <ModalView
+      modalIsOpen={modalIsOpen}
+      closeModal={closeModal}
+      >
+        <p>test</p>
+      </ModalView>
+      <p key={key} onClick={openModal}>{channelData.roomName}</p>
+    </Container>
+  );
 };
