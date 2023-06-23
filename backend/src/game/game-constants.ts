ede7608eb53674fb20dec9ace8e6e2a47db842a7
nori <nori623p@gmail.com>
2023-06-23 13:36:53 +0200

    反射確認用。あとでrevertする

--- backend/src/game/game-constants.ts
+++ backend/src/game/game-constants.ts
@@ -5,6 +5,8 @@ export const CreateBall = (): Ball => structuredClone(ballConstants);
 const ballConstants = {
   x: 0.5,
   y: 0.5,
-  dx: 0.0025,
-  dy: 0.00125,
+  // dx: 0.0025,
+  // dy: 0.00125,
+  dx: 0,
+  dy: 0.025,
 };
