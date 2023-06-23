4f72b4da25e6d8a84e218a8d904089612632d21d
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-06-24 08:57:32 +0900

    input の背景色コメントアウト、ダサいので、

--- frontend/src/components/Elements/Input/Input.tsx
+++ frontend/src/components/Elements/Input/Input.tsx
@@ -28,7 +28,7 @@ export const Input = ({
         sx={{
           '& .MuiInputBase-input': {
             color: '#33ff33', // Text color
-            backgroundColor: '#303030', // 背景色
+            // backgroundColor: '#303030', // 背景色
           },
           '& .MuiInput-underline:after': {
             borderBottomColor: '#33ff33', // 通常時のボーダー色
