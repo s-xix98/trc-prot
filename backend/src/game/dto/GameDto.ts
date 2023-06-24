a3d3b6eb2cc25e3777295a85f654775edaaeca06
nori <nori623p@gmail.com>
2023-06-24 08:24:49 +0200

    gameDtoにpaddleを追加

--- backend/src/game/dto/GameDto.ts
+++ backend/src/game/dto/GameDto.ts
@@ -1,7 +1,10 @@
-import { Ball } from '../types';
+import { Ball, Paddle } from '../types';
 
 type BallDto = Omit<Ball, 'dx' | 'dy'>; // TODO lodash
+type PaddleDto = Omit<Paddle, 'speed'>;
 
 export type GameDto = {
   ball: BallDto;
+  leftPaddle: PaddleDto;
+  rightPaddle: PaddleDto;
 };
