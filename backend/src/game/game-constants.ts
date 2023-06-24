4041467724a25cc53e8cf2149f7a8bc9c977257d
nori <nori623p@gmail.com>
2023-06-24 08:29:13 +0200

    paddle初期化用のファクトリと定数を追加

--- backend/src/game/game-constants.ts
+++ backend/src/game/game-constants.ts
@@ -1,10 +1,26 @@
-import { Ball } from './types.d';
+import { Ball, Paddle } from './types.d';
 
 export const CreateBall = (): Ball => structuredClone(ballConstants);
+
+export const CreatePaddle = (x: number): Paddle => {
+  return {
+    x: x == 0 ? x : x - paddleConstants.width,
+    y: 0.5 - paddleConstants.height / 2,
+    height: paddleConstants.height,
+    width: paddleConstants.width,
+    speed: paddleConstants.speed,
+  };
+};
 
 const ballConstants = {
   x: 0.5,
   y: 0.5,
   dx: 0.0025,
   dy: 0.00125,
+};
+
+const paddleConstants = {
+  height: 0.25,
+  width: 0.025,
+  speed: 0.1,
 };
