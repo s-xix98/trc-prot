0e62322387a449869e2cae65acf36edbe4197ac9
nori <nori623p@gmail.com>
2023-06-25 03:59:51 +0200

    上下キーに対応したアクションを追加

--- backend/src/game/logic/KeyAction.ts
+++ backend/src/game/logic/KeyAction.ts
@@ -1,6 +1,19 @@
+import { Paddle } from '../types';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type KeyAction = (...param: any) => void;
 
 export enum Keys {
   Up = 0,
   Down = 1,
 }
 
+export const keyActions: KeyAction[] = [];
+
+keyActions[Keys.Up] = (paddle: Paddle) => {
+  paddle.y = Math.max(paddle.y - paddle.speed, 0);
+};
+
+keyActions[Keys.Down] = (paddle: Paddle) => {
+  paddle.y = Math.min(paddle.y + paddle.speed, 1 - paddle.height);
+};
