65f71e08b0846cc4d4b8eef8238d87dc5f5475b8
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-06-26 01:17:53 +0900

    python run test

---
+++ frontend/test/e2e.py
@@ -0,0 +1,28 @@
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+TEST_IMG_DIR = "test/img"
+
+import time
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch()
+    context = browser.new_context()
+    page = context.new_page()
+
+    # ログインしてない状態だと、login に 飛ばされるはず
+    page.goto("http://localhost:3000/")
+    page.screenshot(path=f"{TEST_IMG_DIR}/1.png")
+
+    # とりあえず適当にログイン
+    page.get_by_role("button", name="login as fuga").click()
+    time.sleep(3)
+    page.screenshot(path=f"{TEST_IMG_DIR}/2.png")
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
