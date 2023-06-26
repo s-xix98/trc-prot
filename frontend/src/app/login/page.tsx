b4bc736129d5fa2a6af48ca97caf48710fbad95e
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-06-27 02:24:48 +0900

    login or SignUp どちらかのみ表示するようにした

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,24 +1,42 @@
 'use client';
 
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import { useState } from 'react';
+
 import { MainLayout } from '@/components/Layout/MainLayout';
 import { useRedirectToHome } from '@/hooks/useLogin';
-
 import { LoginForm } from '@/features/user/components/LoginForm';
 import { SignUpForm } from '@/features/user/components/SignUpForm';
+
 
 // TODO : UI 改善する
 export default function Login() {
   useRedirectToHome();
 
+  const [isLogin, setValue] = useState(true);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: boolean) => {
+    setValue(newValue);
+  };
+
   return (
     <MainLayout>
-      <h1>Login</h1>
+      <Box sx={{ width: '100%' }}>
+        <Tabs
+          value={isLogin}
+          onChange={handleChange}
+          textColor="inherit"
+          TabIndicatorProps={{ style: { backgroundColor: 'green' } }}
+        >
+          <Tab value={true} label="Login" />
+          <Tab value={false} label="SignUp" />
+        </Tabs>
+      </Box>
       <br />
-      <h2>LoginForm</h2>
-      <LoginForm />
-      <br />
-      <h2>SignUpForm</h2>
-      <SignUpForm />
+      {isLogin && <LoginForm />}
+      {!isLogin && <SignUpForm />}
     </MainLayout>
   );
 }
