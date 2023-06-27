06beb88fd46e9ef3bae4dffe8ad52a6696eaac34
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-06-27 20:54:30 +0900

    自分の表示と他人の表示を分けたます

--- frontend/src/features/user/components/UserProfile.tsx
+++ frontend/src/features/user/components/UserProfile.tsx
@@ -1,13 +1,33 @@
+import { useCurrentUser } from '@/hooks/useCurrentUser';
+
 import { UserInfo } from '../types/UserDto';
 
-export const UserProfile = ({ userInfo }: { userInfo: UserInfo }) => {
+const MyProfile = ({ userInfo }: { userInfo: UserInfo }) => {
   return (
-    <div>
-      <h1>{userInfo?.username}</h1>
-      <hr />
-      <br />
-      <button>fake : send frend req</button>
-      <button>fake : send block req</button>
-    </div>
+    <>
+      <h1>My : {userInfo.username}</h1>
+    </>
+  );
+};
+
+const OtherProfile = ({ userInfo }: { userInfo: UserInfo }) => {
+  return (
+    <>
+      <h1>{userInfo.username}</h1>
+    </>
+  );
+};
+
+export const UserProfile = ({ userInfo }: { userInfo: UserInfo }) => {
+  const currentUser = useCurrentUser();
+
+  return (
+    <>
+      {currentUser.userInfo?.id === userInfo.id ? (
+        <MyProfile userInfo={userInfo} />
+      ) : (
+        <OtherProfile userInfo={userInfo} />
+      )}
+    </>
   );
 };
