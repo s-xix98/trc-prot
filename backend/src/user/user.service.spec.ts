c282ecdc04b156d4643aad147a3487e7c033c39d
wyoheiii <seikimatu4649@gmail.com>
2023-06-28 06:59:35 +0900

    blockuserを取得するテストを追加

--- backend/src/user/user.service.spec.ts
+++ backend/src/user/user.service.spec.ts
@@ -54,4 +54,31 @@ describe('UserService', () => {
     });
   });
 
+
+  describe('ブロック', () => {
+    test('testUser[0]のブロックリストの取得', async () => {
+      for (let i = 1; i < USERNUM; i++) {
+        await prismaService.friendship.upsert({
+          where: {
+            srcUserId_destUserId: {
+              srcUserId: testUsers[0].user.id,
+              destUserId: testUsers[i].user.id,
+            },
+          },
+          update: {
+            status: 'Blocked',
+          },
+          create: {
+            srcUserId: testUsers[0].user.id,
+            destUserId: testUsers[i].user.id,
+            status: 'Blocked',
+          },
+        });
+      }
+
+      const friends = await userService.getBlockUsers(testUsers[0].user.id);
+
+      expect(friends.length).toEqual(USERNUM - 1);
+    });
+  });
 });
