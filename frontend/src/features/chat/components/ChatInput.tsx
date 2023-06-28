9a16e03fe2de17b050ab971da09da8931d8afa3d
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-06-28 15:22:17 +0900

    改行入力できるようにした

--- frontend/src/features/chat/components/ChatInput.tsx
+++ frontend/src/features/chat/components/ChatInput.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { ChangeEvent } from 'react';
+import { KeyboardEvent } from 'react';
 
 import { Input } from '@/components/Elements/Input/Input';
 
@@ -14,26 +14,27 @@ export const ChatInput = ({
   const [msg, setMsg] = useState('');
   const sendMessage = useSendMessage();
 
-  const onChangeAct = (e: ChangeEvent<HTMLInputElement>) => {
-    // TODO : Shift + Enter で 改行を入れられるようにとかしたい
-    if (e.target.value.slice(-1) === '\n') {
-      if (e.target.value.length === 1) {
-        return;
-      }
-      sendBtnAct();
+  const onKeyDownAct = (e: KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === 'Enter' && e.shiftKey) {
       return;
     }
-    setMsg(e.target.value);
-  };
-
-  const sendBtnAct = () => {
-    sendMessage.emit(selectedChannel.id, msg);
-    setMsg('');
+    if (e.key === 'Enter') {
+      e.preventDefault(); // 改行を入力しない
+      sendMessage.emit(selectedChannel.id, msg);
+      setMsg('');
+    }
   };
 
   return (
     <>
-      <Input msg={msg} start={'> '} onChangeAct={onChangeAct} />
+      <Input
+        msg={msg}
+        start={'> '}
+        onKeyDownAct={onKeyDownAct}
+        onChangeAct={(e) => {
+          setMsg(e.target.value);
+        }}
+      />
     </>
   );
 };
