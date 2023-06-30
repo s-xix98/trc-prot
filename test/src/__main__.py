2eaf44ae1902c9a590552ca672ee7adeaa6ee876
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-07-01 08:15:04 +0900

    fmt

--- test/src/__main__.py
+++ test/src/__main__.py
@@ -2,12 +2,8 @@ from src.constants import TOP_PAGE_URL
 from src.logger import logger
 from src.playwright_runner import playwright_test_runner
 from src.test_basic import test_basic
-from src.test_chat import (
-    test_chat_scroll_to_the_bottom,
-    test_chat_send_msg,
-    test_chat_send_some_msg,
-    test_create_channel,
-)
+from src.test_chat import (test_chat_scroll_to_the_bottom, test_chat_send_msg,
+                           test_chat_send_some_msg, test_create_channel)
 from src.test_login import test_login, test_logout, test_signup
 
 
