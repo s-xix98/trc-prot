fdfd1bd98482e2f943214c5c2b00d2cb4d09b21b
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-07-01 05:06:52 +0900

    add UserPageController class

--- test/src/test.py
+++ test/src/test.py
@@ -1,8 +1,17 @@
 from dataclasses import dataclass
 
+from playwright.sync_api import Page
+
 
 @dataclass
 class User:
     name: str
     email: str
     password: str
+
+
+@dataclass
+class UserPageController:
+    test_name: str
+    user: User
+    page: Page
