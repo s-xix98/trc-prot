facf55c18f0381955621ada48b54089ae566d73e
wyoheiii <seikimatu4649@gmail.com>
2023-07-01 04:48:14 +0900

    setup test

--- backend/src/chat/chat.service.spec.ts
+++ backend/src/chat/chat.service.spec.ts
@@ -3,17 +3,31 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TestModule } from '../test/test.module';
 
 import { ChatService } from './chat.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { TestService } from '../test/test.service';
+import { testUser } from '../test/types/test.types';
+
+const USERNUM = 10;
 
 describe('ChatService', () => {
-  let service: ChatService;
+  let chatService: ChatService;
+  let testService: TestService;
+  let testUser: testUser[] = [];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [TestModule],
-      providers: [ChatService],
+      providers: [ChatService, PrismaService, TestService],
     }).compile();
 
-    service = module.get<ChatService>(ChatService);
+    chatService = module.get<ChatService>(ChatService);
+    testService = module.get<TestService>(TestService);
+
+    testUser = await testService.createTestUsersWithSockets(USERNUM);
+  });
+
+  afterAll(async () => {
+    await testService.cleanupDatabase(['chatRoom', 'user']);
   });
 
   test('should be defined', () => {
