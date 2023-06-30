f06518274c278a8fca954b2acdae46d96c5e9b2e
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-06-30 17:10:52 +0900

    fmt

--- frontend/src/features/user/components/Blocks.stories.tsx
+++ frontend/src/features/user/components/Blocks.stories.tsx
@@ -3,13 +3,12 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Blocks } from './Blocks';
 
 const meta = {
-    component: Blocks,
-    tags: ['autodocs'],
+  component: Blocks,
+  tags: ['autodocs'],
 } satisfies Meta<typeof Blocks>;
 
 export default meta;
 
 type Story = StoryObj<typeof Blocks>;
 
-export const Basic: Story = {
-};
+export const Basic: Story = {};
--- frontend/src/features/user/components/Friends.stories.tsx
+++ frontend/src/features/user/components/Friends.stories.tsx
@@ -3,8 +3,8 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Friends } from './Friends';
 
 const meta = {
-    component: Friends,
-    tags: ['autodocs'],
+  component: Friends,
+  tags: ['autodocs'],
 } satisfies Meta<typeof Friends>;
 
 export default meta;
