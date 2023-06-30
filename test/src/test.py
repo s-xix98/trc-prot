296af8380bae3d9a5206d7bbfbb64c940216d689
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-07-01 05:13:53 +0900

    add screenshot func

--- test/src/test.py
+++ test/src/test.py
@@ -1,7 +1,10 @@
+import time
+
 from dataclasses import dataclass
 
 from playwright.sync_api import Page
 
+from src.playwright_runner_utils import take_screenshot
 from src.constants import TOP_PAGE_URL
 
 
@@ -17,6 +20,16 @@ class UserPageController:
     test_name: str
     user: User
     page: Page
+
+    def screenshot(self, act_name: str) -> None:
+        time.sleep(0.3)
+        take_screenshot(
+            self.page,
+            self.test_name,
+            f"{str(self.screenshot_counter)}-{act_name.replace(' ', '-').replace('_', '-')}",
+        )
+        time.sleep(0.3)
+        self.screenshot_counter += 1
 
     def goto_top_page(self) -> None:
         page = self.page
