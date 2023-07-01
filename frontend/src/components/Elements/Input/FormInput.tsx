4eb5800b4a29dba954715b6a9c6c5fc26f1889a4
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-07-01 19:15:55 +0900

    form用のinput作成, loginで使用

---
+++ frontend/src/components/Elements/Input/FormInput.tsx
@@ -0,0 +1,44 @@
+import { InputAdornment, TextField } from '@mui/material';
+import { Controller, useFormContext } from 'react-hook-form';
+
+import { sx } from '@/lib/mui';
+
+export const FormInput = ({
+  name,
+  type,
+  start,
+  placeholder,
+}: {
+  name: string;
+  type: 'text' | 'password';
+  start?: string;
+  placeholder?: string;
+}) => {
+  const { control } = useFormContext();
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      rules={{ required: true }}
+      render={({ field, fieldState }) => (
+        <TextField
+          {...field}
+          type={type}
+          variant="standard"
+          placeholder={placeholder}
+          sx={sx}
+          InputProps={{
+            startAdornment: start ? (
+              <InputAdornment position="start">
+                <p style={{ color: '#33ff33' }}>{start}</p>
+              </InputAdornment>
+            ) : undefined,
+          }}
+          error={fieldState.error ? true : false}
+          helperText={fieldState.error?.message}
+        />
+      )}
+    />
+  );
+};
