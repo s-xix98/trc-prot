e0ee25c5efeda5eeef9b317bb57df632cbddfcdf
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-07-02 19:50:13 +0900

    fmt lint

--- frontend/src/features/user/components/SignUpForm.stories.tsx
+++ frontend/src/features/user/components/SignUpForm.stories.tsx
@@ -1,10 +1,6 @@
-import { expect } from '@storybook/jest';
 import type { Meta, StoryObj } from '@storybook/react';
-import { userEvent, within } from '@storybook/testing-library';
 
 import { SignUpForm } from './SignUpForm';
-
-const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 const meta = {
   component: SignUpForm,
