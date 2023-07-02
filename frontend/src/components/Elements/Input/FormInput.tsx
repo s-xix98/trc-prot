83d7ae4a12cd3a57df6d1d088ba8545009dbf132
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-07-02 19:35:56 +0900

    form 用の input を作成

---
+++ frontend/src/components/Elements/Input/FormInput.tsx
@@ -0,0 +1,48 @@
+import { InputAdornment, TextField } from '@mui/material';
+import { Controller, useFormContext } from 'react-hook-form';
+
+import { inputSx } from '@/lib/mui';
+
+export const FormInput = ({
+  name,
+  type,
+  start,
+  placeholder,
+}: {
+  name: string;
+  type: 'text' | 'password';
+  start?: string;
+  placeholder?: string;
+}) => {
+  const { control, register } = useFormContext();
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      rules={{ required: true }}
+      render={({ fieldState }) => (
+        <TextField
+          {...register(name)}
+          type={type}
+          variant="standard"
+          placeholder={placeholder}
+          sx={inputSx}
+          InputProps={{
+            startAdornment: start ? (
+              <InputAdornment position="start">
+                <p style={{ color: '#33ff33', whiteSpace: 'pre' }}>{start}</p>
+              </InputAdornment>
+            ) : undefined,
+            style: {
+              fontFamily: 'Courier New',
+            },
+          }}
+          autoComplete="off"
+          error={fieldState.error ? true : false}
+          helperText={fieldState.error?.message}
+        />
+      )}
+    />
+  );
+};
