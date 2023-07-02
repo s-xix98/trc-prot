a3bb424f69390c77f93ce97da792958f61cde2e4
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-07-02 19:15:08 +0900

    fix lint

--- frontend/src/features/user/components/LoginForm.stories.tsx
+++ frontend/src/features/user/components/LoginForm.stories.tsx
@@ -1,10 +1,6 @@
-import { expect } from '@storybook/jest';
 import type { Meta, StoryObj } from '@storybook/react';
-import { userEvent, within } from '@storybook/testing-library';
 
 import { LoginForm } from './LoginForm';
-
-const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 const meta = {
   component: LoginForm,
