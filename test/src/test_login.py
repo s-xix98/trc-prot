f877cdf326784d775963dba0325ba41fb6ea2e0f
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-07-02 19:45:01 +0900

    update test

--- test/src/test_login.py
+++ test/src/test_login.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import Page
 
-from src.user import E2E
+from src.user import E2E, User
 from src.user_action import UserInteractionManager
 
 
@@ -10,11 +10,27 @@ def test_signup(page: Page, test_name: str) -> None:
     e2e.signup()
 
 
+def test_signup_invalid_form(page: Page, test_name: str) -> None:
+    invalid_email_user = User(
+        name="invalid-email", email="invalid", password="invalid-email-password"
+    )
+    invalid_email = UserInteractionManager(test_name, invalid_email_user, page)
+    invalid_email.goto_top_page()
+    invalid_email.signup()
+
+
 def test_login(page: Page, test_name: str) -> None:
     e2e = UserInteractionManager(test_name, E2E, page)
     e2e.goto_top_page()
     e2e.login(take_screenshot=True)
     e2e.screenshot("login after")
+
+
+def test_login_invalid_password(page: Page, test_name: str) -> None:
+    e2e = UserInteractionManager(test_name, E2E, page)
+    e2e.goto_top_page()
+
+    e2e.login(password="1", take_screenshot=True)
 
 
 def test_logout(page: Page, test_name: str) -> None:
