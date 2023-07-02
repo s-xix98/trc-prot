c032a143dc48fe6dc8d3239b15221fef0e3f21fc
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-07-02 18:41:24 +0900

    add test_signup_invalid_form test

--- test/src/test_login.py
+++ test/src/test_login.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import Page
 
-from src.user import E2E
+from src.user import E2E, User
 from src.user_action import UserInteractionManager
 
 
@@ -8,6 +8,15 @@ def test_signup(page: Page, test_name: str) -> None:
     e2e = UserInteractionManager(test_name, E2E, page)
     e2e.goto_top_page()
     e2e.signup()
+
+
+def test_signup_invalid_form(page: Page, test_name: str) -> None:
+    invalid_email_user = User(
+        name="invalid-email", email="invalid", password="invalid-email-password"
+    )
+    invalid_email = UserInteractionManager(test_name, invalid_email_user, page)
+    invalid_email.goto_top_page()
+    invalid_email.signup()
 
 
 def test_login(page: Page, test_name: str) -> None:
