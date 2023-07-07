80c2b4b79076e28b99092365fa7ec391816444f2
wyoheiii <seikimatu4649@gmail.com>
2023-07-07 15:28:57 +0900

    確認用にfrontの実装をした

--- frontend/src/app/2fa/page.tsx
+++ frontend/src/app/2fa/page.tsx
@@ -4,9 +4,12 @@ import { useState } from 'react';
 import Image from 'next/image';
 
 import { useSessionAxios } from '@/hooks/useSessionAxios';
+import { tokenStorage } from "@/utils/tokenStorage";
 
 export default function Fa() {
   const [qrcode, setqrcode] = useState<string>('');
+  const [confirm, setConfirm] = useState<string>('');
+  const [auth, setAuth] = useState<string>('');
 
   const axios = useSessionAxios();
   const sendGenerate = () => {
@@ -21,10 +24,54 @@ export default function Fa() {
       });
   };
 
+  const sendConfirm = () => {
+    const dto = {
+      twoFaCode: confirm,
+    }
+    axios.post('/auth/2fa/confirm', dto).then((res) => {
+      console.log(res);
+    }).catch((err) => {
+      console.log(err);
+    });
+  };
+
+  const sendAuth = () => {
+    const dto = {
+      twoFaCode: auth,
+    }
+    axios.post('/auth/2fa/authentication', dto).then((res) => {
+      console.log(res);
+      tokenStorage.set(res.data.jwt);
+    }).catch((err) => {
+      console.log(err);
+    });
+  };
+
+  const sendCheck = () => {
+    axios.get('/auth/2fa/check').then((res) => {
+      console.log(res);
+    }).catch((err) => {
+      console.log(err);
+    });
+  };
+
   return (
     <div>
       {qrcode && <Image src={qrcode} alt="QR Code" width={200} height={200} />}
       <button onClick={sendGenerate}>2fa/generate</button>
+      <input
+          value={confirm}
+          onChange={(event) => setConfirm(event.target.value)}
+        />
+        <button onClick={sendConfirm}>confirm</button>
+
+        <input
+          value={auth}
+          onChange={(event) => setAuth(event.target.value)}
+        />
+        <button onClick={sendAuth}>authentication</button>
+        <button onClick={sendCheck}>check</button>
+
     </div>
   );
 }
