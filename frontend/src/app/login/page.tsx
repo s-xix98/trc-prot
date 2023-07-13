6b373ae1838f457509c9ab37fb36f8a002855f9d
nori <nori623p@gmail.com>
2023-07-14 00:31:50 +0200

    ログイン済みのときも一瞬ログインページ出ちゃうのを直した気分 戻るボタンの挙動が視覚的にわかりやすくなったかも

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -6,14 +6,20 @@ import Box from '@mui/material/Box';
 import { useState } from 'react';
 
 import { MainLayout } from '@/components/Layout/MainLayout';
-import { useRedirectToHome } from '@/hooks/useLogin';
 import { LoginForm } from '@/features/user/components/LoginForm';
 import { SignUpForm } from '@/features/user/components/SignUpForm';
+import { useCurrentUser } from '@/hooks/useCurrentUser';
+import { useDelayedPageTransition } from '@/hooks/useDelayedPageTransition';
+
+// TODO tmp
+const Pien = () => {
+  useDelayedPageTransition('/', 1000);
+  return <h1>🥺</h1>;
+};
 
 // TODO : UI 改善する
 export default function Login() {
-  useRedirectToHome();
-
+  const { userInfo } = useCurrentUser();
   const [isLogin, setValue] = useState(true);
 
   const handleChange = (event: React.SyntheticEvent, newValue: boolean) => {
@@ -21,21 +27,26 @@ export default function Login() {
   };
 
   return (
-    <MainLayout>
-      <Box sx={{ width: '100%' }}>
-        <Tabs
-          value={isLogin}
-          onChange={handleChange}
-          textColor="inherit"
-          TabIndicatorProps={{ style: { backgroundColor: 'green' } }}
-        >
-          <Tab value={true} label="Login" />
-          <Tab value={false} label="SignUp" />
-        </Tabs>
-      </Box>
-      <br />
-      {isLogin && <LoginForm />}
-      {!isLogin && <SignUpForm />}
-    </MainLayout>
+    <>
+      {!userInfo && (
+        <MainLayout>
+          <Box sx={{ width: '100%' }}>
+            <Tabs
+              value={isLogin}
+              onChange={handleChange}
+              textColor="inherit"
+              TabIndicatorProps={{ style: { backgroundColor: 'green' } }}
+            >
+              <Tab value={true} label="Login" />
+              <Tab value={false} label="SignUp" />
+            </Tabs>
+          </Box>
+          <br />
+          {isLogin && <LoginForm />}
+          {!isLogin && <SignUpForm />}
+        </MainLayout>
+      )}
+      {userInfo && <Pien />}
+    </>
   );
 }
