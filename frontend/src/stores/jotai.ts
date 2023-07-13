b0e837356ed2301edbce008bda4805eebf9c7d9f
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-07-13 16:36:29 +0900

    socketにauth token セットしてバックエンドでsocketのユーザーが誰かわかるように ログイン前にauth token セットできないので、atomで管理するようにした

--- frontend/src/stores/jotai.ts
+++ frontend/src/stores/jotai.ts
@@ -1,8 +1,10 @@
 import { atom } from 'jotai';
+import { Socket } from 'socket.io-client';
 
 import { UserInfo } from '../features/user/types/UserDto';
 import { chatChannelDto } from '../features/chat/types/chatChannelDto';
 
 // TODO : ファイル名をかもうちょい考える, いったん移動
 export const userInfoAtom = atom<UserInfo | undefined>(undefined);
+export const socketAtom = atom<Socket | undefined>(undefined);
 export const channelListAtom = atom<chatChannelDto[]>([]);
