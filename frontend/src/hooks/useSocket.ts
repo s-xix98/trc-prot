dd04d5377082c85eee6e9d9257de118d5ce5fa93
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-07-13 16:37:47 +0900

    useSocket で sessionSocket を使用

--- frontend/src/hooks/useSocket.ts
+++ frontend/src/hooks/useSocket.ts
@@ -1,16 +1,23 @@
 'use client';
 
 import { useEffect } from 'react';
+import { useAtomValue } from 'jotai';
 
-import { socket } from '@/socket';
+import { socketAtom } from '@/stores/jotai';
 
 // eslint-disable-next-line
 export const useSocket = (ev: string, listener: (...args: any[]) => void) => {
+  const sessionSocket = useAtomValue(socketAtom);
+
   useEffect(() => {
-    socket.on(ev, listener);
+    if (sessionSocket === undefined) {
+      return;
+    }
+
+    sessionSocket.on(ev, listener);
 
     return () => {
-      socket.off(ev, listener);
+      sessionSocket.off(ev, listener);
     };
-  }, [ev, listener]);
+  }, [ev, listener, sessionSocket]);
 };
