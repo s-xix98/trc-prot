1e8ae2b0fa56661ef50b3d2197b65a7f002d802a
wyoheiii <seikimatu4649@gmail.com>
2023-07-13 18:19:18 +0900

    確認用のfront実装した

--- frontend/src/app/2fa/page.tsx
+++ frontend/src/app/2fa/page.tsx
@@ -7,6 +7,7 @@ import { useSessionAxios } from '@/hooks/useSessionAxios';
 
 export default function Fa() {
   const [qrcode, setqrcode] = useState<string>('');
+  const [confirm, setConfirm] = useState<string>('');
 
   const axios = useSessionAxios();
   const sendGenerate = () => {
@@ -21,10 +22,27 @@ export default function Fa() {
       });
   };
 
+  const sendConfirm = () => {
+    const dto = {
+      twoFaCode: confirm,
+    }
+    axios.post('/auth/2fa/confirm', dto).then((res) => {
+      console.log(res);
+    }).catch((err) => {
+      console.log(err);
+    });
+  };
+
   return (
     <div>
       {qrcode && <Image src={qrcode} alt="QR Code" width={200} height={200} />}
       <button onClick={sendGenerate}>2fa/generate</button>
+
+      <input
+          value={confirm}
+          onChange={(event) => setConfirm(event.target.value)}
+        />
+        <button onClick={sendConfirm}>confirm</button>
     </div>
   );
 }
