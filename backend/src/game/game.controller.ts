aec018227d8f9f8f77cffeef188955e996b95035
nori <nori623p@gmail.com>
2023-07-15 08:00:01 +0200

    テスト落ちるので相対パスに変更

--- backend/src/game/game.controller.ts
+++ backend/src/game/game.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
-import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+
+import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 
 import { Rate } from './types';
 import { GameService } from './game.service';
