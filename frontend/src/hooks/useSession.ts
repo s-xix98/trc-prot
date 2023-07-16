e8506da8e96187a38d9299d21fd629809ae68c9a
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-07-16 12:47:45 +0900

    useSessionのリファクタ

--- frontend/src/hooks/useSession.ts
+++ frontend/src/hooks/useSession.ts
@@ -1,40 +1,34 @@
 import { useEffect } from 'react';
-import { useSetAtom } from 'jotai';
 import { usePathname } from 'next/navigation';
-import { useRouter } from 'next/navigation';
-import { io } from 'socket.io-client';
 
-import { useSessionAxios } from '@/hooks/useSessionAxios';
-import { BACKEND } from '@/constants';
-import { socketAtom, userInfoAtom } from '@/stores/jotai';
 import { tokenStorage } from '@/utils/tokenStorage';
+import {
+  useLogin,
+  useLogout,
+  useVerifySession,
+} from '@/features/user/api/userLogin';
 
-import { UserInfo } from '../features/user/types/UserDto';
+import { useCurrentUser } from './useCurrentUser';
 
 export const useSession = () => {
-  const axios = useSessionAxios();
-  const setUserInfo = useSetAtom(userInfoAtom);
-  const setSocket = useSetAtom(socketAtom);
   const pathname = usePathname();
-  const router = useRouter();
+  const { automaticLogin } = useLogin();
+  const logout = useLogout();
+  const { verifySession } = useVerifySession();
+  const { currentUserInfo } = useCurrentUser();
 
   useEffect(() => {
     // tokenStorage がない時は、/login に飛ぶはずなので、
     // /user/me に api 投げるまでもない
     if (tokenStorage.get() === null) {
-      // TODO : login に飛ばす処理統一させる
-      setUserInfo(undefined);
-      router.push('/login');
+      logout();
       return;
     }
-    axios
-      .get<UserInfo>(BACKEND + '/user/me')
-      .then((res) => {
-        setUserInfo(res.data);
-        setSocket(io(BACKEND, { auth: { token: tokenStorage.get() } }));
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [axios, setUserInfo, setSocket, pathname, router]);
+
+    if (!currentUserInfo) {
+      automaticLogin();
+    } else {
+      verifySession();
+    }
+  }, [pathname, currentUserInfo, automaticLogin, logout, verifySession]);
 };
