7d2cc56199e8f416377b055dc7ca95c24b2dd9b5
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-07-17 07:03:24 +0900

    channelListAtomを駆逐

--- frontend/src/stores/jotai.ts
+++ frontend/src/stores/jotai.ts
@@ -2,9 +2,7 @@ import { atom } from 'jotai';
 import { Socket } from 'socket.io-client';
 
 import { CurrentUser } from '../features/user/types/UserDto';
-import { chatChannelDto } from '../features/chat/types/chatChannelDto';
 
 // TODO : ファイル名をかもうちょい考える, いったん移動
 export const currentUserAtom = atom<CurrentUser | undefined>(undefined);
 export const socketAtom = atom<Socket | undefined>(undefined);
-export const channelListAtom = atom<chatChannelDto[]>([]);
