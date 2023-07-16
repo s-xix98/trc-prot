97a7f1c7a13c6a9fcb512c4776d6bae3a7892a75
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-07-16 12:44:29 +0900

    login処理のリファクタ automaticLoginとmanualLoginにわけた

--- frontend/src/features/user/api/userLogin.ts
+++ frontend/src/features/user/api/userLogin.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useAtom, useSetAtom } from 'jotai';
 import { useRouter } from 'next/navigation';
+import { io } from 'socket.io-client';
 
 import { tokenStorage } from '@/utils/tokenStorage';
 import { accessToken } from '@/app/login/types/accessToken';
@@ -14,16 +15,37 @@ import { BACKEND } from '../../../constants';
 export const useLogin = () => {
   const setUserInfo = useSetAtom(userInfoAtom);
   const sessionAxios = useSessionAxios();
+  const setSocket = useSetAtom(socketAtom);
 
-  const login = async (email: string, pass: string) => {
+  const automaticLogin = () => {
+    sessionAxios
+      .get<UserInfo>('/user/me')
+      .then((res) => {
+        setUserInfo(res.data);
+        setSocket(io(BACKEND, { auth: { token: tokenStorage.get() } }));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const manualLogin = (email: string, pass: string) => {
     const loginDto: LoginDto = { email: email, hashedPassword: pass };
 
-    try {
-      const res = await axios.post<accessToken>(
-        BACKEND + '/auth/login',
-        loginDto,
-      );
-      tokenStorage.set(res.data.jwt);
+    axios
+      .post<accessToken>(BACKEND + '/auth/login', loginDto)
+      .then((res) => {
+        tokenStorage.set(res.data.jwt);
+        automaticLogin();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return { automaticLogin, manualLogin };
+};
+
 
       const user = await sessionAxios.get<UserInfo>('/user/me');
       setUserInfo(user.data);
