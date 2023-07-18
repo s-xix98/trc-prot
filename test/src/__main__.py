709ca1ac147b83dd426b6ae248bedebf231a787c
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-07-18 10:23:42 +0900

    add test case

--- test/src/__main__.py
+++ test/src/__main__.py
@@ -4,8 +4,10 @@ from src.playwright_runner import playwright_test_runner
 from src.test_basic import test_basic
 from src.test_chat import (test_chat_scroll_to_the_bottom, test_chat_send_msg,
                            test_chat_send_some_msg, test_create_channel)
-from src.test_login import (test_login, test_login_invalid_password,
-                            test_logout, test_signup, test_signup_invalid_form)
+from src.test_login import (test_login, test_login_incorrect_password,
+                            test_login_invalid_password, test_logout,
+                            test_signup, test_signup_already_use_email,
+                            test_signup_invalid_form)
 
 
 def main() -> None:
@@ -17,6 +19,8 @@ def main() -> None:
             test_signup_invalid_form,
             test_login,
             test_login_invalid_password,
+            test_login_incorrect_password,
+            test_signup_already_use_email,
             test_logout,
             test_create_channel,
             test_chat_send_msg,
