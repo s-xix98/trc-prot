6b27d0ca0217edc4e6c10d3db2b187d24cb8e662
nori <nori623p@gmail.com>
2023-07-18 09:45:56 +0200

    refactor: 改行消した

--- backend/src/game/game.service.ts
+++ backend/src/game/game.service.ts
@@ -8,22 +8,16 @@ import { MatchHistory } from './dto/Api';
 @Injectable()
 export class GameService {
   constructor(private readonly prismaService: PrismaService) {}
-
   async GetRanking() {
     const ranking = await this.prismaService.rating.findMany({
       select: {
-        userData: {
-          select: {
-            username: true,
-          },
-        },
+        userData: { select: { username: true } },
         userId: true,
         rating: true,
       },
-      orderBy: {
-        rating: 'desc',
-      },
+      orderBy: { rating: 'desc' },
     });
+
     return ranking.map((r) => ({
       ...pick(r, 'userId', 'rating'),
       ...r.userData,
@@ -32,18 +26,10 @@ export class GameService {
 
   async UpdateRating(userId: string, result: 'WIN' | 'LOSE') {
     const rankingDiff = result === 'WIN' ? 1 : -1;
-
     await this.prismaService.rating.upsert({
-      where: {
-        userId: userId,
-      },
-      update: {
-        rating: { increment: rankingDiff },
-      },
-      create: {
-        userId: userId,
-        rating: rankingDiff,
-      },
+      where: { userId: userId },
+      update: { rating: { increment: rankingDiff } },
+      create: { userId: userId, rating: rankingDiff },
     });
   }
 
@@ -51,39 +37,13 @@ export class GameService {
     const history: MatchHistory[] =
       await this.prismaService.matchHistory.findMany({
         select: {
-          player1: {
-            select: {
-              id: true,
-              username: true,
-            },
-          },
-          player2: {
-            select: {
-              id: true,
-              username: true,
-            },
-          },
-          winner: {
-            select: {
-              id: true,
-              username: true,
-            },
-          },
+          player1: { select: { id: true, username: true } },
+          player2: { select: { id: true, username: true } },
+          winner: { select: { id: true, username: true } },
           createdAt: true,
         },
-        where: {
-          OR: [
-            {
-              player1Id: userId,
-            },
-            {
-              player2Id: userId,
-            },
-          ],
-        },
-        orderBy: {
-          createdAt: 'desc',
-        },
+        where: { OR: [{ player1Id: userId }, { player2Id: userId }] },
+        orderBy: { createdAt: 'desc' },
       });
 
     console.log('history raw', history);
@@ -96,11 +56,7 @@ export class GameService {
     winnerId?: string,
   ) {
     await this.prismaService.matchHistory.create({
-      data: {
-        player1Id: userId1,
-        player2Id: userId2,
-        winnerId: winnerId,
-      },
+      data: { player1Id: userId1, player2Id: userId2, winnerId: winnerId },
     });
   }
 }
