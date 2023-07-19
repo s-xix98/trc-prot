0ab9a421d7f834cead6a0ebd61cba09b154c5def
nori <nori623p@gmail.com>
2023-07-19 13:22:14 +0200

    game削除メソッド gameが終了したときとかにマッチングテーブルから削除する用

--- backend/src/game/matching/matching-table.ts
+++ backend/src/game/matching/matching-table.ts
@@ -10,4 +10,16 @@ export class MatchingTable {
     return this.userGameMap.get(userId);
   }
 
+
+  deleteGame(userId1: string, userId2: string): boolean {
+    const g1 = this.getGame(userId1);
+    const g2 = this.getGame(userId2);
+    if (!g1 || !g2 || g1 !== g2) {
+      return false;
+    }
+    const isU1deleted = this.userGameMap.delete(userId1);
+    const isU2deleted = this.userGameMap.delete(userId2);
+    return isU1deleted && isU2deleted;
+  }
+
 }
