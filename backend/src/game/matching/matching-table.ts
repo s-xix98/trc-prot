59c5a0b31e9626958b0d78df69439a2f03799773
nori <nori623p@gmail.com>
2023-07-19 13:20:56 +0200

    ゲーム取得用メソッド ユーザー再接続時のソケット変更用

--- backend/src/game/matching/matching-table.ts
+++ backend/src/game/matching/matching-table.ts
@@ -5,4 +5,9 @@ import { GameLogic } from '../logic/game-logic';
 export class MatchingTable {
   private userGameMap = new Map<string, GameLogic>();
   private waitingUser: PlayerData | undefined = undefined;
+
+  getGame(userId: string): GameLogic | undefined {
+    return this.userGameMap.get(userId);
+  }
+
 }
