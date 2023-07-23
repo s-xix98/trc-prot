8c055ed8db8413e64d749091988fcabd045dfe17
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-07-23 14:48:17 +0900

    trace, video を保存可能に

--- test/src/constants.py
+++ test/src/constants.py
@@ -4,11 +4,13 @@ from typing import Final
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--TAKE_SCREENSHOT", default=1, type=int)
+parser.add_argument("--TRACE", default=0, type=int)
 parser.add_argument("--HEADLESS", default=1, type=int)
 
 args = parser.parse_args()
 
 TAKE_SCREENSHOT = True if args.TAKE_SCREENSHOT != 0 else False
+TRACE = True if args.TRACE != 0 else False
 HEADLESS = True if args.HEADLESS != 0 else False
 
 TEST_IMG_DIR: Final = "img"
