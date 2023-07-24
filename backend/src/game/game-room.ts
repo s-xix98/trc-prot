144e03902c76bb4a217006276ac4b3e6602f5147
nori <nori623p@gmail.com>
2023-07-24 19:19:16 +0200

    src === destの招待は受け付けない

--- backend/src/game/game-room.ts
+++ backend/src/game/game-room.ts
@@ -21,6 +21,9 @@ export class GameRoom {
     if (this.isPlaying(p2.data.id)) {
       return { err: p2.data.username + ' already playing' };
     }
+    if (p1.data.id === p2.data.id) {
+      return { err: 'same user' };
+    }
     const game = gameFactory(p1, p2);
     this.playingUsers.set(p1.data.id, game);
     this.playingUsers.set(p2.data.id, game);
@@ -30,7 +33,7 @@ export class GameRoom {
   delete({ p1, p2 }: { p1: UserId; p2: UserId }): boolean {
     const g1 = this.getGame(p1);
     const g2 = this.getGame(p2);
-    if (!g1 || !g2 || g1 !== g2) {
+    if (!g1 || !g2 || g1 !== g2 || p1 == p2) {
       return false;
     }
     const isU1deleted = this.playingUsers.delete(p1);
