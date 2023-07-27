5dc2225ccc59105631b3a6c8ce69f14e7e59ca3e
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-07-27 13:53:30 +0900

    招待されてるチャンネルか確認するやつを作った

--- frontend/src/hooks/useCurrentUser.ts
+++ frontend/src/hooks/useCurrentUser.ts
@@ -90,11 +90,15 @@ export const useFriendStatus = () => {
 };
 
 export const useChatRoomStatus = () => {
-  const { joinedRooms } = useCurrentUser();
+  const { joinedRooms, receiveInviteChatRooms } = useCurrentUser();
 
   const isJoinedRoom = (room: chatChannelDto) => {
     return joinedRooms.some((r) => r.id === room.id);
   };
 
-  return { isJoinedRoom };
+  const isInvitedRoom = (room: chatChannelDto) => {
+    return receiveInviteChatRooms.some((r) => r.chatRoom.id === room.id);
+  };
+
+  return { isJoinedRoom, isInvitedRoom };
 };
