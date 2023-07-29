35671538f0e04430a66cfcfbfb5a39522d5a08a5
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-07-29 17:21:39 +0900

    ちょー雑に friend 出すところで オンラインとかのstate表示

--- frontend/src/features/user/components/Friends.tsx
+++ frontend/src/features/user/components/Friends.tsx
@@ -1,14 +1,26 @@
 import { useCurrentUser } from '@/hooks/useCurrentUser';
 
-import { UserListWithModal } from './UserProfile';
+import { UserProfileModal } from './UserProfile';
+import { useUserProfileModal } from '@/hooks/useUserProfileModal';
 
 export const Friends = () => {
   const { friends } = useCurrentUser();
+  const { selectingUser, modal, openUserProfileModal } = useUserProfileModal();
 
   return (
     <>
       <h1>Friends</h1>
-      <UserListWithModal userList={friends} />
+      <UserProfileModal userInfo={selectingUser} {...modal} />
+      {friends.map((user, idx) => (
+        <p
+          key={idx}
+          onClick={() => {
+            openUserProfileModal(user);
+          }}
+        >
+          {user.username} {user.state}
+        </p>
+      ))}
     </>
   );
 };
