ad42c680c544b940ffbe7c8f6e2793a0fe3793f1
wyoheiii <seikimatu4649@gmail.com>
2023-07-30 19:57:16 +0900

    optionalに変更

--- frontend/src/features/chat/types/chatChannelDto.ts
+++ frontend/src/features/chat/types/chatChannelDto.ts
@@ -3,8 +3,8 @@ import { z } from 'zod';
 export const chatChannelSchema = z.object({
   id: z.string(),
   roomName: z.string(),
-  isPrivate: z.boolean(),
-  hasPassword: z.boolean(),
+  isPrivate: z.boolean().optional(),
+  hasPassword: z.boolean().optional(),
 });
 export type chatChannelDto = z.infer<typeof chatChannelSchema>;
 
