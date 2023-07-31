b61f910fae2dfb50c0bf0a003decd255c3bace29
s-xix98 <77755127+s-xix98@users.noreply.github.com>
2023-07-31 10:46:17 +0900

    fmt

--- frontend/src/features/chat/components/ChatTalkAreaHeader.tsx
+++ frontend/src/features/chat/components/ChatTalkAreaHeader.tsx
@@ -1,15 +1,15 @@
-import { ModalView } from '@/components/Elements/Modal/ModalView';
-import { useModal } from '@/hooks/useModal';
-
-import { chatChannelDto } from '../types/chatChannelDto';
-
-import { ChannelInfo } from './ChannelInfo';
-import { Container } from '@/components/Layout/Container';
-import { useUpdateChatRoom } from '../api/updateChatRoom';
 import { IconButton } from '@mui/material';
-
 import PublicIcon from '@mui/icons-material/Public';
 import PublicOffIcon from '@mui/icons-material/PublicOff';
+
+import { ModalView } from '@/components/Elements/Modal/ModalView';
+import { useModal } from '@/hooks/useModal';
+import { Container } from '@/components/Layout/Container';
+
+import { chatChannelDto } from '../types/chatChannelDto';
+import { useUpdateChatRoom } from '../api/updateChatRoom';
+
+import { ChannelInfo } from './ChannelInfo';
 
 
 const ShowPrivateState = ({
@@ -55,19 +55,19 @@ export const ChatTalkAreaHeader = ({
         <ChannelInfo selectedChannel={selectedChannel} />
       </ModalView>
       <div>
-      <Container>
-      <h2
-        onClick={() => {
-          modal.openModal();
-        }}
-        style={{ margin: 'auto auto auto 3px' }}
-      >
-        ChatTalkArea {selectedChannel.roomName}
-      </h2>
-      <div style={{ margin: 'auto 10px auto auto' }}>
-        <ShowPrivateState selectedChannel={selectedChannel} />
-      </div>
-      </Container>
+        <Container>
+          <h2
+            onClick={() => {
+              modal.openModal();
+            }}
+            style={{ margin: 'auto auto auto 3px' }}
+          >
+            ChatTalkArea {selectedChannel.roomName}
+          </h2>
+          <div style={{ margin: 'auto 10px auto auto' }}>
+            <ShowPrivateState selectedChannel={selectedChannel} />
+          </div>
+        </Container>
       </div>
     </>
   );
