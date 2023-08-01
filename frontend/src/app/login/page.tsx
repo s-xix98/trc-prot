12b1662394f25ba71057c939b94d0c9dbbcfc287
wyoheiii <seikimatu4649@gmail.com>
2023-08-01 23:40:34 +0900

    loginエリアに世界遺産を設置

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -9,6 +9,8 @@ import { MainLayout } from '@/components/Layout/MainLayout';
 import { useRedirectToHome } from '@/hooks/useLogin';
 import { LoginForm } from '@/features/user/components/LoginForm';
 import { SignUpForm } from '@/features/user/components/SignUpForm';
+import { AuthLogin } from '@/features/user/components/AuthLogin';
+import { BACKEND } from '@/constants';
 
 // TODO : UI 改善する
 export default function Login() {
@@ -31,11 +33,15 @@ export default function Login() {
         >
           <Tab value={0} label="Login" />
           <Tab value={1} label="SignUp" />
+          <Tab value={2} label="42Auth" />
+          <Tab value={3} label="GoogleAuth" />
         </Tabs>
       </Box>
       <br />
       {isLogin === 0 && <LoginForm />}
       {isLogin === 1 && <SignUpForm />}
+      {isLogin === 2 && <AuthLogin displayName={"42"} redirectUrl={BACKEND + "/auth/42"} />}
+      {isLogin === 3 && <AuthLogin displayName={"google"} redirectUrl={BACKEND + "/auth/google"}/>}
     </MainLayout>
   );
 }
